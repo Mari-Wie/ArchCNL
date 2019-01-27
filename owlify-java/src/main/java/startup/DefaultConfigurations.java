cb17bdefef650d25ef16f8000841748ee263b0c8
sandra.schroeder <schroeder@informatik.uni-hamburg.de>
2019-01-27 16:58:49 +0100

    moved java code ontology to source code

--- owlify-java/src/main/java/startup/DefaultConfigurations.java
+++ owlify-java/src/main/java/startup/DefaultConfigurations.java
@@ -1,16 +1,13 @@
 package startup;
 
 public class DefaultConfigurations {
-	
+
 	public static OntologyExtractionConfiguration defaultConfig() {
-		String ontologyPath = "C:\\Users\\sandr\\Documents\\repositories\\Promotion\\DL_formalizations\\ontologies\\ontologies_unified\\";
+		String ontologyPath = "./ontology/javacodeontology.owl";
 
 		OntologyExtractionConfiguration config = OntologyExtractionConfiguration.newOntologyExtractionConfiguration()
-				.withCodeOntology(
-						ontologyPath+"javacodeontology.owl")
-				.withOutput("./result.owl")
-				.withSourceCode(
-						"C:\\Users\\sandr\\Documents\\workspaces\\workspace_cnl\\ExamplePlugin\\src\\")
+				.withCodeOntology(ontologyPath).withOutput("./result.owl")
+				.withSourceCode("C:\\Users\\sandr\\Documents\\workspaces\\workspace_cnl_test\\TestProject\\src")
 				.withCodeOntologyURI("http://arch-ont.org/ontologies/javacodeontology.owl#");
 		return config;
 	}
--- owlify-java/src/main/java/startup/Startup.java
+++ owlify-java/src/main/java/startup/Startup.java
@@ -7,9 +7,7 @@ public class Startup {
 	public static void main(String[] args) {
 
 		OntologyExtractionConfiguration config = DefaultConfigurations.defaultConfig();
-
 		OntologyJavaParser parser = new OntologyJavaParser(config);
 		parser.execute();
 	}
-
 }
