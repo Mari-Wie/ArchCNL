3b3448f120056b721da4309e40a366d99414a3a7
Mari-Wie <wiese@informatik.uni-hamburg.de>
2020-05-31 19:31:56 +0200

    Init

--- src/main/java/CNLToolchain.java
+++ src/main/java/CNLToolchain.java
@@ -18,79 +18,103 @@ import impl.StardogDatabase;
 import impl.StardogDatabase.StardogDatabaseBuilder;
 import parser.FamixOntologyTransformer;
 
-public class CNLToolchain {
+public class CNLToolchain
+{
 
-	private List<String> ontologyPaths;
-	//private OwlifyComponent javaOWLTransformer;
-	private OwlifyComponent famixTransformer;
-	private ExecuteMappingAPI mappingAPI;
-	private StardogICVAPI icvAPI;
-	
-	private String databaseName;
-	private String server; 
+    private List<String> ontologyPaths;
+    //private OwlifyComponent javaOWLTransformer;
+    private OwlifyComponent famixTransformer;
+    private ExecuteMappingAPI mappingAPI;
+    private StardogICVAPI icvAPI;
 
-	public CNLToolchain(String databaseName, String server) {
-		this.databaseName = databaseName;
-		this.server = server;
-		this.icvAPI = StardogAPIFactory.getICVAPI();
-		this.famixTransformer = new FamixOntologyTransformer();
-	}
-	
-	public void execute(String docPath, String sourceCodePath, String context)
-			throws MissingBuilderArgumentException, FileNotFoundException, NoConnectionToStardogServerException {
-		AsciiDocArc42Parser parser = new AsciiDocArc42Parser();
-		parser.parseRulesFromDocumentation(docPath);
-		parser.parseMappingRulesFromDocumentation(docPath);
-		ontologyPaths = parser.getOntologyPaths();
-		
+    private String databaseName;
+    private String server;
 
-		// Source Code Transformation
-		famixTransformer.addSourcePath(sourceCodePath);
-		famixTransformer.transform();
-		//javaOWLTransformer = new JavaCodeOntologyAPIImpl();
-		//javaOWLTransformer.setSource(sourceCodePath);
-		//javaOWLTransformer.transform();
+    public CNLToolchain(String databaseName, String server)
+    {
+        this.databaseName = databaseName;
+        this.server = server;
+        this.icvAPI = StardogAPIFactory.getICVAPI();
+        this.famixTransformer = new FamixOntologyTransformer();
+    }
 
-		// Mapping
-		mappingAPI = ExecuteMappingAPIFactory.get();
-		ReasoningConfiguration reasoningConfig = ReasoningConfiguration.build().addPathsToConcepts(ontologyPaths)
-				.withMappingRules(parser.getMappingFilePath()).withData(famixTransformer.getResultPath());
-		mappingAPI.setReasoningConfiguration(reasoningConfig);
-		mappingAPI.executeMapping();
+    public void execute(String docPath, String sourceCodePath, String context)
+            throws MissingBuilderArgumentException, FileNotFoundException,
+            NoConnectionToStardogServerException
+    {
+        AsciiDocArc42Parser parser = new AsciiDocArc42Parser();
+        parser.parseRulesFromDocumentation(docPath);
+        parser.parseMappingRulesFromDocumentation(docPath);
+        ontologyPaths = parser.getOntologyPaths();
 
-		//create stardog db
-		StardogDatabase db = new StardogDatabaseBuilder().server(this.server)
-				.databaseName(databaseName).userName("admin").password("admin").createStardogDatabase();
-		db.connect();
-		// Load code to stardog and perform conformance checking
-		db.addDataByRDFFileAsNamedGraph(mappingAPI.getReasoningResultPath(), context); //TODO ConformanceCheck component?
+        // Source Code Transformation
+        famixTransformer.addSourcePath(sourceCodePath);
+        famixTransformer.transform();
+        //javaOWLTransformer = new JavaCodeOntologyAPIImpl();
+        //javaOWLTransformer.setSource(sourceCodePath);
+        //javaOWLTransformer.transform();
 
-		IConformanceCheck check = new ConformanceCheckImpl(icvAPI);
-		check.createNewConformanceCheck();
-		for (ArchitectureRule rule : ArchitectureRules.getInstance().getRules().keySet()) {
-			check.storeArchitectureRule(rule);
-			check.validateRule(rule, db, context);
-			check.storeConformanceCheckingResultInDatabaseForRule(rule, db, context);
-		}
-	}
-	
-	public static void main(String[] args) {
-		String database = "test";
-		String context = "http://graphs.org/" + database + "/1.0";
-		CNLToolchain tool = new CNLToolchain("test", "http://localhost:5820");
-		try {
-			String projectPath = "C:\\Users\\wiese8\\Uni\\Work\\OnionArchitectureDemo_version1\\";
-			tool.execute(projectPath+"architecture-documentation-onion.adoc", projectPath, context);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (MissingBuilderArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoConnectionToStardogServerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+        // Mapping
+        mappingAPI = ExecuteMappingAPIFactory.get();
+        ReasoningConfiguration reasoningConfig = ReasoningConfiguration.build()
+            .addPathsToConcepts(ontologyPaths)
+            .withMappingRules(parser.getMappingFilePath())
+            .withData(famixTransformer.getResultPath());
+        mappingAPI.setReasoningConfiguration(reasoningConfig);
+        mappingAPI.executeMapping();
+
+        //create stardog db
+        StardogDatabase db = new StardogDatabaseBuilder().server(this.server)
+            .databaseName(databaseName)
+            .userName("admin")
+            .password("admin")
+            .createStardogDatabase();
+        db.connect();
+        // Load code to stardog and perform conformance checking
+        db.addDataByRDFFileAsNamedGraph(mappingAPI.getReasoningResultPath(),
+                context); //TODO ConformanceCheck component?
+
+        IConformanceCheck check = new ConformanceCheckImpl(icvAPI);
+        check.createNewConformanceCheck();
+        for (ArchitectureRule rule : ArchitectureRules.getInstance()
+            .getRules()
+            .keySet())
+        {
+            check.storeArchitectureRule(rule);
+            check.validateRule(rule, db, context);
+            check.storeConformanceCheckingResultInDatabaseForRule(rule, db,
+                    context);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        String database = "MWTest";
+        String context = "http://graphs.org/" + database + "/1.0";
+        System.out.println("Initializing ...");
+        CNLToolchain tool = new CNLToolchain(database, "http://localhost:5820");
+        try
+        {
+            String projectPath = "C:\\WORK\\Kopie_G+J\\Uni\\Work\\OnionArchitectureDemo_version1\\";
+            tool.execute(projectPath + "architecture-documentation-onion.adoc",
+                    projectPath, context);
+            System.out.println("Finished!");
+        }
+        catch (FileNotFoundException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (MissingBuilderArgumentException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (NoConnectionToStardogServerException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 }
