23f5e682886ebd479a6d0f252adda5cc8345b729
cmkehler <christian.kehler@gmx.net>
2021-06-09 15:42:59 +0200

    Issue-127: Add modifiers to local variables
    Added modifiers to the ontology of local variables.
    feat/Issue-127

--- owlify-famix/src/test/java/org/archcnl/owlify/famix/codemodel/LocalVariableTest.java
+++ owlify-famix/src/test/java/org/archcnl/owlify/famix/codemodel/LocalVariableTest.java
@@ -2,6 +2,7 @@ package org.archcnl.owlify.famix.codemodel;
 
 import static org.archcnl.owlify.famix.ontology.FamixOntology.FamixClasses.LocalVariable;
 import static org.archcnl.owlify.famix.ontology.FamixOntology.FamixClasses.Method;
+import static org.archcnl.owlify.famix.ontology.FamixOntology.FamixDatatypeProperties.hasModifier;
 import static org.archcnl.owlify.famix.ontology.FamixOntology.FamixDatatypeProperties.hasName;
 import static org.archcnl.owlify.famix.ontology.FamixOntology.FamixObjectProperties.definesVariable;
 import static org.archcnl.owlify.famix.ontology.FamixOntology.FamixObjectProperties.hasDeclaredType;
@@ -16,6 +17,9 @@ import org.archcnl.owlify.famix.ontology.FamixOntology;
 import org.archcnl.owlify.famix.ontology.FamixOntology.FamixClasses;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LocalVariableTest {
 
@@ -33,7 +37,8 @@ public class LocalVariableTest {
     public void testLocalVariableTransformation() {
         Type type = new Type("Type", "Type", false);
         final String parentUri = "SomeClass.someMethod";
-        LocalVariable variable = new LocalVariable(type, "i");
+        final List<Modifier> modifiers = Arrays.asList(new Modifier("final"));
+        LocalVariable variable = new LocalVariable(type, "i", modifiers);
         Individual method = ontology.createIndividual(Method, parentUri);
 
         variable.modelIn(ontology, parentUri, method);
@@ -54,5 +59,6 @@ public class LocalVariableTest {
                                 individual,
                                 ontology.get(hasDeclaredType),
                                 type.getIndividual(ontology)));
+        assertNotNull(ontology.codeModel().getProperty(individual, ontology.get(hasModifier)));
     }
 }
