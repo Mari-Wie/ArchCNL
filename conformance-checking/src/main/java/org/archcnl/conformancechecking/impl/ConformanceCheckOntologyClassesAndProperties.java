82ecb95b12c3321835c0ef2cb90f311703cafdac
Vincent Raudszus <vincent.raudszus@t-online.de>
2021-06-12 17:33:35 +0200

    Style: Constant renamed in accordance with the Google style guide
    Constants names should be all uppercase letters

--- conformance-checking/src/main/java/org/archcnl/conformancechecking/impl/ConformanceCheckOntologyClassesAndProperties.java
+++ conformance-checking/src/main/java/org/archcnl/conformancechecking/impl/ConformanceCheckOntologyClassesAndProperties.java
@@ -7,7 +7,7 @@ import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 
 public class ConformanceCheckOntologyClassesAndProperties {
-    private static final String namespace =
+    private static final String NAMESPACE =
             "http://arch-ont.org/ontologies/architectureconformance#";
     private static int violationId;
     private static int proofId;
@@ -15,104 +15,104 @@ public class ConformanceCheckOntologyClassesAndProperties {
     private static int notInferredId;
 
     public static String getOntologyNamespace() {
-        return namespace;
+        return NAMESPACE;
     }
 
     public static Individual getConformanceCheckIndividual(OntModel model) {
-        OntClass conformanceClass = model.getOntClass(namespace + "ConformanceCheck");
-        return model.createIndividual(namespace + "ConformanceCheck", conformanceClass);
+        OntClass conformanceClass = model.getOntClass(NAMESPACE + "ConformanceCheck");
+        return model.createIndividual(NAMESPACE + "ConformanceCheck", conformanceClass);
     }
 
     public static DatatypeProperty getDateProperty(OntModel model) {
-        return model.getDatatypeProperty(namespace + "hasCheckingDate");
+        return model.getDatatypeProperty(NAMESPACE + "hasCheckingDate");
     }
 
     public static Individual getArchitectureRuleIndividual(OntModel model, int id) {
-        OntClass ruleClass = model.getOntClass(namespace + "ArchitectureRule");
-        return model.createIndividual(namespace + "ArchitectureRule" + id, ruleClass);
+        OntClass ruleClass = model.getOntClass(NAMESPACE + "ArchitectureRule");
+        return model.createIndividual(NAMESPACE + "ArchitectureRule" + id, ruleClass);
     }
 
     public static DatatypeProperty getCNLRepresentationProperty(OntModel model) {
-        return model.getDatatypeProperty(namespace + "hasRuleRepresentation");
+        return model.getDatatypeProperty(NAMESPACE + "hasRuleRepresentation");
     }
 
     public static ObjectProperty getValidatesProperty(OntModel model) {
-        return model.getObjectProperty(namespace + "validates");
+        return model.getObjectProperty(NAMESPACE + "validates");
     }
 
     public static DatatypeProperty getHasRuleIDProperty(OntModel model) {
 
-        return model.getDatatypeProperty(namespace + "hasRuleID");
+        return model.getDatatypeProperty(NAMESPACE + "hasRuleID");
     }
 
     public static Individual getArchitectureViolationIndividual(OntModel model) {
-        OntClass violationClass = model.getOntClass(namespace + "ArchitectureViolation");
+        OntClass violationClass = model.getOntClass(NAMESPACE + "ArchitectureViolation");
         return model.createIndividual(
-                namespace + "ArchitectureViolation" + violationId++, violationClass);
+                NAMESPACE + "ArchitectureViolation" + violationId++, violationClass);
     }
 
     public static ObjectProperty getViolatesProperty(OntModel model) {
-        return model.getObjectProperty(namespace + "violates");
+        return model.getObjectProperty(NAMESPACE + "violates");
     }
 
     public static ObjectProperty getHasViolationProperty(OntModel model) {
-        return model.getObjectProperty(namespace + "hasViolation");
+        return model.getObjectProperty(NAMESPACE + "hasViolation");
     }
 
     public static ObjectProperty getHasDetectedViolationProperty(OntModel model) {
-        return model.getObjectProperty(namespace + "hasDetected");
+        return model.getObjectProperty(NAMESPACE + "hasDetected");
     }
 
     public static DatatypeProperty getHasViolationTextProperty(OntModel model) {
-        return model.getDatatypeProperty(namespace + "hasViolationText");
+        return model.getDatatypeProperty(NAMESPACE + "hasViolationText");
     }
 
     public static DatatypeProperty getHasRuleTypeProperty(OntModel model) {
-        return model.getDatatypeProperty(namespace + "hasRuleType");
+        return model.getDatatypeProperty(NAMESPACE + "hasRuleType");
     }
 
     public static Individual getProofIndividual(OntModel model) {
-        OntClass proofClass = model.getOntClass(namespace + "Proof");
-        return model.createIndividual(namespace + "Proof" + proofId++, proofClass);
+        OntClass proofClass = model.getOntClass(NAMESPACE + "Proof");
+        return model.createIndividual(NAMESPACE + "Proof" + proofId++, proofClass);
     }
 
     public static ObjectProperty getProofsProperty(OntModel model) {
-        return model.getObjectProperty(namespace + "proofs");
+        return model.getObjectProperty(NAMESPACE + "proofs");
     }
 
     public static DatatypeProperty getProofsTextProperty(OntModel model) {
-        return model.getDatatypeProperty(namespace + "hasProofText");
+        return model.getDatatypeProperty(NAMESPACE + "hasProofText");
     }
 
     public static Individual getAssertedStatement(OntModel model) {
-        OntClass statementClass = model.getOntClass(namespace + "AssertedStatement");
+        OntClass statementClass = model.getOntClass(NAMESPACE + "AssertedStatement");
         return model.createIndividual(
-                namespace + "AssertedStatement" + assertedId++, statementClass);
+                NAMESPACE + "AssertedStatement" + assertedId++, statementClass);
     }
 
     public static Individual getNotInferredStatement(OntModel model) {
-        OntClass statementClass = model.getOntClass(namespace + "NotInferredStatement");
+        OntClass statementClass = model.getOntClass(NAMESPACE + "NotInferredStatement");
         return model.createIndividual(
-                namespace + "NotInferredStatement" + notInferredId++, statementClass);
+                NAMESPACE + "NotInferredStatement" + notInferredId++, statementClass);
     }
 
     public static ObjectProperty getHasSubject(OntModel model) {
-        return model.getObjectProperty(namespace + "hasSubject");
+        return model.getObjectProperty(NAMESPACE + "hasSubject");
     }
 
     public static ObjectProperty getHasPredicate(OntModel model) {
-        return model.getObjectProperty(namespace + "hasPredicate");
+        return model.getObjectProperty(NAMESPACE + "hasPredicate");
     }
 
     public static ObjectProperty getHasObject(OntModel model) {
-        return model.getObjectProperty(namespace + "hasObject");
+        return model.getObjectProperty(NAMESPACE + "hasObject");
     }
 
     public static ObjectProperty getHasAssertedStatement(OntModel model) {
-        return model.getObjectProperty(namespace + "hasAssertedStatement");
+        return model.getObjectProperty(NAMESPACE + "hasAssertedStatement");
     }
 
     public static ObjectProperty getHasNotInferredStatement(OntModel model) {
-        return model.getObjectProperty(namespace + "hasNotInferredStatement");
+        return model.getObjectProperty(NAMESPACE + "hasNotInferredStatement");
     }
 }
