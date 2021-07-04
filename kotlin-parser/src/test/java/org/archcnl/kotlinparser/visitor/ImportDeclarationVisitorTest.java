418df327370ab51e768735ff77eca67338a1dbb3
AlexHamburg <oleksandr.trunov@gmail.com>
2021-07-04 17:50:18 +0200

    ARCHCNL-12: Rename test based on pattern given-when-then

--- kotlin-parser/src/test/java/org/archcnl/kotlinparser/visitor/ImportDeclarationVisitorTest.java
+++ kotlin-parser/src/test/java/org/archcnl/kotlinparser/visitor/ImportDeclarationVisitorTest.java
@@ -1,28 +1,27 @@
 package org.archcnl.kotlinparser.visitor;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.io.IOException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class ImportDeclarationVisitorTest {
 
-    @Test
-    void testImportListFromComplexClass() throws IOException {
-        var namedFileContext = TestHelper.getKotlinFileContextFromFile("ComplexClass.kt");
-
-        var importDeclarationVisitor =
-                new ImportDeclarationVisitor(namedFileContext.getRulesNames());
-        importDeclarationVisitor.visit(namedFileContext.getFileContext());
-
-        var imports = importDeclarationVisitor.getImports();
-
-        assertEquals(3, imports.size());
-        assertEquals("example.subpackage.ClassInSubpackage", imports.get(0).getName());
-        assertEquals("ClassInSubpackage", imports.get(0).getSimpleName());
-        assertEquals("example.anotherPackage.AnotherClass", imports.get(1).getName());
-        assertEquals("AnotherClass", imports.get(1).getSimpleName());
-        assertEquals("java.util", imports.get(2).getName());
-        assertEquals("util", imports.get(2).getSimpleName());
-    }
+  @Test
+  void givenKotlinClass_whenImportDeclarationVisitorVisit_thenImportsFound() throws IOException {
+    // given
+    final var namedFileContext = TestHelper.getKotlinFileContextFromFile(TestHelper.COMPLEX_CLASS);
+    final var importDeclarationVisitor =
+        new ImportDeclarationVisitor(namedFileContext.getRulesNames());
+    // when
+    importDeclarationVisitor.visit(namedFileContext.getFileContext());
+    // then
+    final var imports = importDeclarationVisitor.getImports();
+    Assertions.assertEquals(3, imports.size());
+    Assertions.assertEquals("example.subpackage.ClassInSubpackage", imports.get(0).getName());
+    Assertions.assertEquals("ClassInSubpackage", imports.get(0).getSimpleName());
+    Assertions.assertEquals("example.anotherPackage.AnotherClass", imports.get(1).getName());
+    Assertions.assertEquals("AnotherClass", imports.get(1).getSimpleName());
+    Assertions.assertEquals("java.util", imports.get(2).getName());
+    Assertions.assertEquals("util", imports.get(2).getSimpleName());
+  }
 }
--- kotlin-parser/src/test/java/org/archcnl/kotlinparser/visitor/NamespaceVisitorTest.java
+++ kotlin-parser/src/test/java/org/archcnl/kotlinparser/visitor/NamespaceVisitorTest.java
@@ -1,37 +1,36 @@
 package org.archcnl.kotlinparser.visitor;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.io.IOException;
 import org.archcnl.owlify.famix.codemodel.Namespace;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class NamespaceVisitorTest {
-    @Test
-    public void testNamespaceFromComplexClass() throws IOException {
-        var namedFileContext = TestHelper.getKotlinFileContextFromFile("ComplexClass.kt");
+  @Test
+  public void testNamespaceFromComplexClass() throws IOException {
+    final var namedFileContext = TestHelper.getKotlinFileContextFromFile(TestHelper.COMPLEX_CLASS);
 
-        var namespaceVisitor = new NamespaceVisitor(namedFileContext.getRulesNames());
-        namespaceVisitor.visit(namedFileContext.getFileContext());
+    final var namespaceVisitor = new NamespaceVisitor(namedFileContext.getRulesNames());
+    namespaceVisitor.visit(namedFileContext.getFileContext());
 
-        var namespace = namespaceVisitor.getNamespace();
+    final var namespace = namespaceVisitor.getNamespace();
 
-        assertEquals("example", namespace.getName());
-        assertEquals(Namespace.TOP, namespace.getParent());
-    }
+    Assertions.assertEquals("example", namespace.getName());
+    Assertions.assertEquals(Namespace.TOP, namespace.getParent());
+  }
 
-    @Test
-    public void testNamespaceFromClassInSubpackage() throws IOException {
-        var namedFileContext =
-                TestHelper.getKotlinFileContextFromFile("subpackage", "ClassInSubpackage.kt");
+  @Test
+  public void testNamespaceFromClassInSubpackage() throws IOException {
+    final var namedFileContext = TestHelper.getKotlinFileContextFromFile(TestHelper.SUBPACKAGE,
+        TestHelper.CLASS_IN_SUBPACKAGE);
 
-        var namespaceVisitor = new NamespaceVisitor(namedFileContext.getRulesNames());
-        namespaceVisitor.visit(namedFileContext.getFileContext());
+    final var namespaceVisitor = new NamespaceVisitor(namedFileContext.getRulesNames());
+    namespaceVisitor.visit(namedFileContext.getFileContext());
 
-        var namespace = namespaceVisitor.getNamespace();
+    final var namespace = namespaceVisitor.getNamespace();
 
-        assertEquals("example.subpackage", namespace.getName());
-        assertEquals("example", namespace.getParent().getName());
-        assertEquals(Namespace.TOP, namespace.getParent().getParent());
-    }
+    Assertions.assertEquals("example.subpackage", namespace.getName());
+    Assertions.assertEquals("example", namespace.getParent().getName());
+    Assertions.assertEquals(Namespace.TOP, namespace.getParent().getParent());
+  }
 }
