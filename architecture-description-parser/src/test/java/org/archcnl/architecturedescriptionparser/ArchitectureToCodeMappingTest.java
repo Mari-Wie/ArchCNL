05113e10a0605b3fb5268896dca8225938596d51
cmkehler <christian.kehler@gmx.net>
2021-07-07 17:22:07 +0200

    Refactored ArchitectureToCodeMappingTest

--- architecture-description-parser/src/test/java/org/archcnl/architecturedescriptionparser/ArchitectureToCodeMappingTest.java
+++ architecture-description-parser/src/test/java/org/archcnl/architecturedescriptionparser/ArchitectureToCodeMappingTest.java
@@ -5,8 +5,9 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Arrays;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class ArchitectureToCodeMappingTest {
 
@@ -15,25 +16,30 @@ public class ArchitectureToCodeMappingTest {
             "foobar: (?class rdf:type test:Foo) (?class2 rdf:type test:Bar) -> (?class test:qux ?class2)";
     private static final String rule2 =
             "abc: (?a test:qux test:A) (?a ?b test:B) (?c test:qux test:C)  -> (?a ?b ?c)";
-    private static final String expectation = prefix + "[" + rule1 + "]\n[" + rule2 + "]\n";
-
-    private ArchitectureToCodeMapping mapping;
-
-    @Before
-    public void setUp() {
-        mapping = new ArchitectureToCodeMapping(prefix, Arrays.asList(rule1, rule2));
-    }
+    private static final String expectation = prefix + "[" + rule1 + "]\n[" + rule2 + "]\n"; 
+    
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
-    public void testAsSWRL() throws IOException {
+    public void givenArchitectureToCodeMapping_whenReturnedAsSWRL_thenSWRLEqualsExpectation() throws IOException {
+    	//given
+    	ArchitectureToCodeMapping mapping = new ArchitectureToCodeMapping(prefix, Arrays.asList(rule1, rule2));
+    	
+    	//when, then
         assertEquals(expectation, mapping.asSWRL());
     }
 
     @Test
-    public void testWrite() throws IOException {
-        StringWriter writer = new StringWriter();
-        mapping.write(writer);
+    public void givenArchitectureToCodeMapping_whenWrittenToAStringWriter_thenStringWriterToStringEqualsExpectation() throws IOException {
+    	//given
+    	ArchitectureToCodeMapping mapping = new ArchitectureToCodeMapping(prefix, Arrays.asList(rule1, rule2));
+    	StringWriter writer = new StringWriter();
+        
+    	//when
+    	mapping.write(writer);
 
+    	//then
         assertEquals(expectation, writer.toString());
     }
 }
