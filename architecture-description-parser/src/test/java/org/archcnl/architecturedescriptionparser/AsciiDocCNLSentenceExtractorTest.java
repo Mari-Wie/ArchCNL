2680a9978b1bbf8c6c166c6e8eb0423b381af309
cmkehler <christian.kehler@gmx.net>
2021-08-04 15:37:59 +0200

    Tried a new formatter (eclipse-java-google-style), because of spotless violation

--- architecture-description-parser/src/test/java/org/archcnl/architecturedescriptionparser/AsciiDocCNLSentenceExtractorTest.java
+++ architecture-description-parser/src/test/java/org/archcnl/architecturedescriptionparser/AsciiDocCNLSentenceExtractorTest.java
@@ -8,53 +8,51 @@ import org.junit.Test;
 
 public class AsciiDocCNLSentenceExtractorTest {
 
-	private AsciiDocCNLSentenceExtractor extractor;
+  private AsciiDocCNLSentenceExtractor extractor;
 
-	@Test
-	public void givenArchitectureRules_whenGivenToSentenceExtractor_thenRulesAreExtractedCorrectly() {
-		// given, when
-		extractor = new AsciiDocCNLSentenceExtractor(Paths.get("./src/test/resources/rules.adoc"));
-		// then
-		assertThat(extractor.extractArchitectureRules(), CoreMatchers
-				.hasItems("Only LayerOne can use LayerTwo.", "No LayerTwo can use LayerOne."));
-	}
+  @Test
+  public void givenArchitectureRules_whenGivenToSentenceExtractor_thenRulesAreExtractedCorrectly() {
+    // given, when
+    extractor = new AsciiDocCNLSentenceExtractor(Paths.get("./src/test/resources/rules.adoc"));
+    // then
+    assertThat(extractor.extractArchitectureRules(),
+        CoreMatchers.hasItems("Only LayerOne can use LayerTwo.", "No LayerTwo can use LayerOne."));
+  }
 
-	@Test
-	public void givenMappingRules_whenGivenToSentenceExtractor_thenMappingsAreExtractedCorrectly() {
-		// given, when
-		extractor = new AsciiDocCNLSentenceExtractor(Paths.get("./src/test/resources/rules.adoc"));
-		// then
-		assertThat(extractor.extractMappingRules(), CoreMatchers.hasItems(
-				"useMapping: (?class rdf:type famix:FamixClass) (?class2 rdf:type famix:FamixClass) (?f rdf:type famix:Attribute) (?class famix:definesAttribute ?f) (?f famix:hasDeclaredType ?class2) -> (?class architecture:use ?class2)",
-				"isLayerOne: (?class rdf:type famix:FamixClass) (?class famix:hasName ?name) regex(?name, 'TestLayerOne') -> (?class rdf:type architecture:LayerOne)",
-				"isLayerTwo: (?class rdf:type famix:FamixClass) (?class famix:hasName ?name) regex(?name, 'TestLayerTwo') -> (?class rdf:type architecture:LayerTwo)"));
-	}
+  @Test
+  public void givenMappingRules_whenGivenToSentenceExtractor_thenMappingsAreExtractedCorrectly() {
+    // given, when
+    extractor = new AsciiDocCNLSentenceExtractor(Paths.get("./src/test/resources/rules.adoc"));
+    // then
+    assertThat(extractor.extractMappingRules(), CoreMatchers.hasItems(
+        "useMapping: (?class rdf:type famix:FamixClass) (?class2 rdf:type famix:FamixClass) (?f rdf:type famix:Attribute) (?class famix:definesAttribute ?f) (?f famix:hasDeclaredType ?class2) -> (?class architecture:use ?class2)",
+        "isLayerOne: (?class rdf:type famix:FamixClass) (?class famix:hasName ?name) regex(?name, 'TestLayerOne') -> (?class rdf:type architecture:LayerOne)",
+        "isLayerTwo: (?class rdf:type famix:FamixClass) (?class famix:hasName ?name) regex(?name, 'TestLayerTwo') -> (?class rdf:type architecture:LayerTwo)"));
+  }
 
-	@Test
-	public void givenEmptyArchitectureRules_whenGivenToSentenceExtractor_thenExtractedRulesIsEmpty() {
-		// given, when
-		extractor = new AsciiDocCNLSentenceExtractor(
-				Paths.get("./src/test/resources/emptyrules.adoc"));
-		// then
-		assertTrue(extractor.extractArchitectureRules().isEmpty());
-	}
+  @Test
+  public void givenEmptyArchitectureRules_whenGivenToSentenceExtractor_thenExtractedRulesIsEmpty() {
+    // given, when
+    extractor = new AsciiDocCNLSentenceExtractor(Paths.get("./src/test/resources/emptyrules.adoc"));
+    // then
+    assertTrue(extractor.extractArchitectureRules().isEmpty());
+  }
 
-	@Test
-	public void givenEmptyMappingsRules_whenGivenToSentenceExtractor_thenExtractedRulesIsEmpty() {
-		// given, when
-		extractor = new AsciiDocCNLSentenceExtractor(
-				Paths.get("./src/test/resources/emptyrules.adoc"));
-		// then
-		assertTrue(extractor.extractMappingRules().isEmpty());
-	}
+  @Test
+  public void givenEmptyMappingsRules_whenGivenToSentenceExtractor_thenExtractedRulesIsEmpty() {
+    // given, when
+    extractor = new AsciiDocCNLSentenceExtractor(Paths.get("./src/test/resources/emptyrules.adoc"));
+    // then
+    assertTrue(extractor.extractMappingRules().isEmpty());
+  }
 
-	@Test
-	public void givenNonAsciidocFile_whenGivenToSentenceExtractor_thenExtractorIsEmpty() {
-		// given, when
-		extractor = new AsciiDocCNLSentenceExtractor(
-				Paths.get("./src/test/resources/mapping-expected.txt"));
-		// then
-		assertTrue(extractor.extractArchitectureRules().isEmpty());
-		assertTrue(extractor.extractMappingRules().isEmpty());
-	}
+  @Test
+  public void givenNonAsciidocFile_whenGivenToSentenceExtractor_thenExtractorIsEmpty() {
+    // given, when
+    extractor =
+        new AsciiDocCNLSentenceExtractor(Paths.get("./src/test/resources/mapping-expected.txt"));
+    // then
+    assertTrue(extractor.extractArchitectureRules().isEmpty());
+    assertTrue(extractor.extractMappingRules().isEmpty());
+  }
 }
