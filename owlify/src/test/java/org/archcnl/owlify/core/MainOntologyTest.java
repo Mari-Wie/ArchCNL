6211a0bfb5bad01e3f8ab82b64b3e44ba264dc2a
cmkehler <christian.kehler@gmx.net>
2021-08-09 13:09:34 +0200

    Rebase on master

--- owlify/src/test/java/org/archcnl/owlify/core/MainOntologyTest.java
+++ owlify/src/test/java/org/archcnl/owlify/core/MainOntologyTest.java
@@ -23,85 +23,89 @@ import org.junit.Test;
 
 public class MainOntologyTest {
 
-	private static final String path = "/this/is/an/example.path";
-	private Individual content1;
-	private Individual content2;
-	private MainOntology mainOnt;
+    private static final String path = "/this/is/an/example.path";
+    private Individual content1;
+    private Individual content2;
+    private MainOntology mainOnt;
 
-	@Before
-	public void setUp() throws FileNotFoundException {
-		OntModel myOnt = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
+    @Before
+    public void setUp() throws FileNotFoundException {
+        OntModel myOnt = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
 
-		final OntClass class1 = myOnt.createClass("http://test.owl#ClassA");
-		final OntClass class2 = myOnt.createClass("http://test.owl#ClassB");
+        final OntClass class1 = myOnt.createClass("http://test.owl#ClassA");
+        final OntClass class2 = myOnt.createClass("http://test.owl#ClassB");
 
-		mainOnt = new MainOntology(new FileInputStream("src/main/resources/ontologies/main.owl"));
-		content1 = class1.createIndividual("InstanceA");
-		content2 = class2.createIndividual("InstanceB");
-	}
+        mainOnt = new MainOntology(new FileInputStream("src/main/resources/ontologies/main.owl"));
+        content1 = class1.createIndividual("InstanceA");
+        content2 = class2.createIndividual("InstanceB");
+    }
 
-	@Test
-	public void givenSoftwareArtifacts_whenModeledByMainOntology_thenMainOntologyContainsArtifacts() {
-		// given, when
-		mainOnt.modelSoftwareArtifactFile(path, Arrays.asList(content1, content2));
+    @Test
+    public void
+            givenSoftwareArtifacts_whenModeledByMainOntology_thenMainOntologyContainsArtifacts() {
+        // given, when
+        mainOnt.modelSoftwareArtifactFile(path, Arrays.asList(content1, content2));
 
-		OntModel main = mainOnt.getOntology();
-		Individual artifact = main
-				.getIndividual(MainOntClasses.SoftwareArtifactFile.uri() + "/" + path);
-		ObjectProperty contains = main
-				.getObjectProperty(MainObjectProperties.containsArtifact.uri());
-		DatatypeProperty hasPath = main.getDatatypeProperty(MainDatatypeProperties.hasPath.uri());
+        OntModel main = mainOnt.getOntology();
+        Individual artifact =
+                main.getIndividual(MainOntClasses.SoftwareArtifactFile.uri() + "/" + path);
+        ObjectProperty contains =
+                main.getObjectProperty(MainObjectProperties.containsArtifact.uri());
+        DatatypeProperty hasPath = main.getDatatypeProperty(MainDatatypeProperties.hasPath.uri());
 
-		// then
-		assertNotNull(artifact);
-		assertTrue(mainOnt.getOntology().contains(artifact, contains, content1));
-		assertTrue(mainOnt.getOntology().contains(artifact, contains, content2));
-		assertTrue(mainOnt.getOntology().containsLiteral(artifact, hasPath, path));
-	}
+        // then
+        assertNotNull(artifact);
+        assertTrue(mainOnt.getOntology().contains(artifact, contains, content1));
+        assertTrue(mainOnt.getOntology().contains(artifact, contains, content2));
+        assertTrue(mainOnt.getOntology().containsLiteral(artifact, hasPath, path));
+    }
 
-	@Test
-	public void givenEmptySoftwareArtifact_whenModeledByMainOntology_thenMainOntologyDoesNotContainArtifacts() {
-		// given, when
-		mainOnt.modelSoftwareArtifactFile(path, new ArrayList<Individual>());
+    @Test
+    public void
+            givenEmptySoftwareArtifact_whenModeledByMainOntology_thenMainOntologyDoesNotContainArtifacts() {
+        // given, when
+        mainOnt.modelSoftwareArtifactFile(path, new ArrayList<Individual>());
 
-		OntModel main = mainOnt.getOntology();
-		Individual artifact = main
-				.getIndividual(MainOntClasses.SoftwareArtifactFile.uri() + "/" + path);
-		ObjectProperty contains = main
-				.getObjectProperty(MainObjectProperties.containsArtifact.uri());
-		DatatypeProperty hasPath = main.getDatatypeProperty(MainDatatypeProperties.hasPath.uri());
+        OntModel main = mainOnt.getOntology();
+        Individual artifact =
+                main.getIndividual(MainOntClasses.SoftwareArtifactFile.uri() + "/" + path);
+        ObjectProperty contains =
+                main.getObjectProperty(MainObjectProperties.containsArtifact.uri());
+        DatatypeProperty hasPath = main.getDatatypeProperty(MainDatatypeProperties.hasPath.uri());
 
-		// then
-		assertNotNull(artifact);
-		assertFalse(mainOnt.getOntology().contains(artifact, contains, content1));
-		assertFalse(mainOnt.getOntology().contains(artifact, contains, content2));
-		assertTrue(mainOnt.getOntology().containsLiteral(artifact, hasPath, path));
-	}
+        // then
+        assertNotNull(artifact);
+        assertFalse(mainOnt.getOntology().contains(artifact, contains, content1));
+        assertFalse(mainOnt.getOntology().contains(artifact, contains, content2));
+        assertTrue(mainOnt.getOntology().containsLiteral(artifact, hasPath, path));
+    }
 
-	@Test
-	public void givenOntModel_whenReadingOwlFile_thenOntModelURIsMatchURIsFromOntologyFile()
-			throws FileNotFoundException {
-		// given, when
-		OntModel baseOnt = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
-		baseOnt.read(new FileInputStream("src/main/resources/ontologies/main.owl"), null);
+    @Test
+    public void givenOntModel_whenReadingOwlFile_thenOntModelURIsMatchURIsFromOntologyFile()
+            throws FileNotFoundException {
+        // given, when
+        OntModel baseOnt = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
+        baseOnt.read(new FileInputStream("src/main/resources/ontologies/main.owl"), null);
 
-		// then
-		for (MainOntClasses ontClass : MainOntClasses.values()) {
-			assertNotNull(
-					ontClass.name() + " from MainOntClasses has no associated class in main.owl.",
-					baseOnt.getOntClass(ontClass.uri()));
-		}
+        // then
+        for (MainOntClasses ontClass : MainOntClasses.values()) {
+            assertNotNull(
+                    ontClass.name() + " from MainOntClasses has no associated class in main.owl.",
+                    baseOnt.getOntClass(ontClass.uri()));
+        }
 
-		for (MainObjectProperties prop : MainObjectProperties.values()) {
-			assertNotNull(prop.name()
-					+ " from MainObjectProperties has no associated object property in main.owl.",
-					baseOnt.getObjectProperty(prop.uri()));
-		}
+        for (MainObjectProperties prop : MainObjectProperties.values()) {
+            assertNotNull(
+                    prop.name()
+                            + " from MainObjectProperties has no associated object property in main.owl.",
+                    baseOnt.getObjectProperty(prop.uri()));
+        }
 
-		for (MainDatatypeProperties prop : MainDatatypeProperties.values()) {
-			assertNotNull(prop.name()
-					+ " from MainDatatypeProperties has no associated datatype property in main.owl.",
-					baseOnt.getDatatypeProperty(prop.uri()));
-		}
-	}
+        for (MainDatatypeProperties prop : MainDatatypeProperties.values()) {
+            assertNotNull(
+                    prop.name()
+                            + " from MainDatatypeProperties has no associated datatype property in main.owl.",
+                    baseOnt.getDatatypeProperty(prop.uri()));
+        }
+    }
 }
