b5a7409c3eab8b2981f5df02edff6d8fdc2670e2
Oliver Heidmann <oliver.heidmann@mpimet.mpg.de>
2021-08-22 12:12:44 +0100

    added dummy PauseButton

---
+++ web-ui/src/main/java/org/vaadin/example/PauseButton.java
@@ -0,0 +1,28 @@
+package org.vaadin.example;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+
+class PauseButton extends Button {
+    private Component mainLayout;
+    private VaadinIcon icons[] = new VaadinIcon[] {VaadinIcon.PLAY_CIRCLE, VaadinIcon.PAUSE};
+    private int currentIcon = 1;
+
+    PauseButton(Component mainLayout) {
+        addClickListener(
+                e -> {
+                    e.getSource().setIcon(getNextIcon());
+                });
+        setIcon(getNextIcon());
+    }
+
+    private Icon getNextIcon() {
+        int nextIcon = (currentIcon + 1) % 2;
+        Icon newIcon = new Icon(icons[currentIcon]);
+        currentIcon = nextIcon;
+        return newIcon;
+    }
+}
