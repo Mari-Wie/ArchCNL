e1614949585f7fd3cc6524f89f901b7bc40ff8b4
cmkehler <christian.kehler@gmx.net>
2021-08-25 23:36:41 +0200

    Made main ui block a bit prettier

--- web-ui/src/main/java/org/vaadin/example/MainView.java
+++ web-ui/src/main/java/org/vaadin/example/MainView.java
@@ -1,19 +1,19 @@
-//package org.vaadin.example;
+// package org.vaadin.example;
 //
-//import com.vaadin.flow.component.button.Button;
-//import com.vaadin.flow.component.dependency.CssImport;
-//import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-//import com.vaadin.flow.component.textfield.TextArea;
-//import com.vaadin.flow.component.textfield.TextField;
-//import com.vaadin.flow.data.value.ValueChangeMode;
-//import com.vaadin.flow.router.Route;
-//import com.vaadin.flow.server.PWA;
-//import org.archcnl.stardogwrapper.api.StardogAPIFactory;
-//import org.archcnl.stardogwrapper.api.StardogDatabaseAPI;
-//import org.archcnl.stardogwrapper.api.StardogICVAPI;
-//import org.archcnl.stardogwrapper.impl.StardogDatabase;
+// import com.vaadin.flow.component.button.Button;
+// import com.vaadin.flow.component.dependency.CssImport;
+// import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+// import com.vaadin.flow.component.textfield.TextArea;
+// import com.vaadin.flow.component.textfield.TextField;
+// import com.vaadin.flow.data.value.ValueChangeMode;
+// import com.vaadin.flow.router.Route;
+// import com.vaadin.flow.server.PWA;
+// import org.archcnl.stardogwrapper.api.StardogAPIFactory;
+// import org.archcnl.stardogwrapper.api.StardogDatabaseAPI;
+// import org.archcnl.stardogwrapper.api.StardogICVAPI;
+// import org.archcnl.stardogwrapper.impl.StardogDatabase;
 //
-///**
+/// **
 // * A sample Vaadin view class.
 // *
 // * <p>To implement a Vaadin view just extend any Vaadin component and use @Route annotation to
@@ -22,15 +22,15 @@
 // *
 // * <p>A new instance of this class is created for every new user and every browser tab/window.
 // */
-//@Route
-//@PWA(
+// @Route
+// @PWA(
 //        name = "Vaadin Application",
 //        shortName = "Vaadin App",
 //        description = "This is an example Vaadin application.",
 //        enableInstallPrompt = false)
-//@CssImport("./styles/shared-styles.css")
-//@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
-//public class MainView extends VerticalLayout {
+// @CssImport("./styles/shared-styles.css")
+// @CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+// public class MainView extends VerticalLayout {
 //
 //    /**
 //     * Construct a new Vaadin view.
@@ -109,7 +109,14 @@
 //    public void printQuery() {
 //        System.out.println(query);
 //        String q =
-//                " PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> PREFIX conformance: <http://arch-ont.org/ontologies/architectureconformance#> PREFIX famix: <http://arch-ont.org/ontologies/famix.owl#> PREFIX architecture: <http://www.arch-ont.org/ontologies/architecture.owl#> SELECT DISTINCT ?cnl ?violation WHERE { GRAPH ?g { ?rule rdf:type conformance:ArchitectureRule.  ?rule conformance:hasRuleRepresentation ?cnl.  ?violation conformance:violates ?rule.  } }";
+//                " PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX owl:
+// <http://www.w3.org/2002/07/owl#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX xsd:
+// <http://www.w3.org/2001/XMLSchema#> PREFIX conformance:
+// <http://arch-ont.org/ontologies/architectureconformance#> PREFIX famix:
+// <http://arch-ont.org/ontologies/famix.owl#> PREFIX architecture:
+// <http://www.arch-ont.org/ontologies/architecture.owl#> SELECT DISTINCT ?cnl ?violation WHERE {
+// GRAPH ?g { ?rule rdf:type conformance:ArchitectureRule.  ?rule conformance:hasRuleRepresentation
+// ?cnl.  ?violation conformance:violates ?rule.  } }";
 //        try {
 //            this.db.executeSelectQuery(ta.getValue());
 //        } catch (Exception e) {
@@ -117,4 +124,4 @@
 //            e.printStackTrace();
 //        }
 //    }
-//}
+// }
--- web-ui/src/main/java/org/vaadin/mainview/MainUI.java
+++ web-ui/src/main/java/org/vaadin/mainview/MainUI.java
@@ -2,11 +2,10 @@ package org.vaadin.mainview;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Unit;
-import com.vaadin.flow.component.contextmenu.MenuItem;
-import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.menubar.MenuBar;
-import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -18,65 +17,114 @@ import com.vaadin.flow.server.PWA;
         shortName = "Vaadin App",
         description = "This is an example Vaadin application.",
         enableInstallPrompt = false)
-@CssImport("./styles/shared-styles.css")
-@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+@HtmlImport("./styles/main-style-module.html")
 public class MainUI extends VerticalLayout {
 
-    private Component header, footer;
-    Component content;
+    private Component header, oldContent, footer;
+    private VerticalLayout content;
     private MainUIPresenter presenter;
 
     public MainUI() {
-        presenter = new MainUIPresenter();
-        Component mb = createHeader();
-        Component c = createContent();
-        Component f = createFooter();
-        add(mb);
-        addAndExpand(c);
-        add(f);
+        presenter = new MainUIPresenter(this);
+        header = createHeader();
+        content = createContent();
+        footer = createFooter();
+        setPadding(false);
+
+        add(header);
+        addAndExpand(content);
+        add(footer);
     }
 
     private Component createHeader() {
-        MenuBar menuBar = new MenuBar();
-        menuBar.addItem("Back", e -> presenter.Back());
-        menuBar.addItem("Forward", e -> presenter.Forward());
-        MenuItem project = menuBar.addItem("Project");
-        project.getSubMenu().addItem("Import Project", e -> presenter.importProject());
-        MenuItem rules = menuBar.addItem("Rules");
-        rules.getSubMenu().addItem("Import Rules", e -> presenter.importRules());
-        rules.getSubMenu().addItem("Export Rules", e -> presenter.exportRules());
-        menuBar.addItem("View", e -> presenter.View());
-        menuBar.addItem("Help", e -> presenter.Help());
-        menuBar.getStyle().set("border-style", "solid");
-        menuBar.setWidth(100, Unit.PERCENTAGE);
-        return menuBar;
+        HorizontalLayout headerBox = new HorizontalLayout();
+
+        Button backButton = new Button("Back", e -> presenter.Back());
+        backButton.getStyle().set("background-color", "#76d0f1");
+        backButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+        backButton.getStyle().set("color", "Black");
+
+        Button forwardButton = new Button("Forward", e -> presenter.Forward());
+        forwardButton.getStyle().set("background-color", "#76d0f1");
+        forwardButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+        forwardButton.getStyle().set("color", "Black");
+
+        Button projectButton = new Button("Project", e -> presenter.importProject());
+        projectButton.getStyle().set("background-color", "#76d0f1");
+        projectButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+        projectButton.getStyle().set("padding-right", "10px");
+        projectButton.getStyle().set("color", "Black");
+
+        Button rulesButton = new Button("Rules", e -> presenter.importRules());
+        rulesButton.getStyle().set("background-color", "#76d0f1");
+        rulesButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+        rulesButton.getStyle().set("color", "Black");
+
+        Button viewButton = new Button("View", e -> presenter.view());
+        viewButton.getStyle().set("background-color", "#76d0f1");
+        viewButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+        viewButton.getStyle().set("color", "Black");
+
+        Button helpButton = new Button("Help", e -> presenter.help());
+        helpButton.getStyle().set("background-color", "#76d0f1");
+        helpButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+        helpButton.getStyle().set("color", "Black");
+
+        headerBox.add(
+                backButton, forwardButton, projectButton, rulesButton, viewButton, helpButton);
+        headerBox.setSpacing(false);
+        headerBox.getThemeList().add("spacing-xs");
+
+        headerBox.getStyle().set("background-color", "#2596be");
+        headerBox.setWidthFull();
+        return headerBox;
     }
 
-    private Component createContent() {
+    private VerticalLayout createContent() {
         VerticalLayout vbox = new VerticalLayout();
-        vbox.setHeight(80, Unit.PERCENTAGE);
-        vbox.setSpacing(true);
+        oldContent = presenter.getArchitectureRuleView();
+        vbox.add(oldContent);
         return vbox;
     }
 
     private Component createFooter() {
         HorizontalLayout footerHbox = new HorizontalLayout();
-        Label copyright = new Label("© 2021 University of Hamburg. All rights reserved");
-        MenuBar menuBar = new MenuBar();
-        menuBar.addItem("Contact", e -> presenter.contact());
-        menuBar.addItem("Wiki", e -> presenter.wiki());
-        menuBar.addItem("Project Site", e -> presenter.projectSite());
-        footerHbox.add(copyright, menuBar);
-        // footerHbox.getStyle().set("background-color", "#3458eb");
-        footerHbox.getStyle().set("border-style", "solid");
-        footerHbox.setHeight(10, Unit.PERCENTAGE);
-        footerHbox.setWidth(100, Unit.PERCENTAGE);
 
+        Label copyright = new Label("© 2021 University of Hamburg. All rights reserved");
+        copyright.setWidth(87, Unit.PERCENTAGE);
+        copyright.getStyle().set("font-family", "Montserrat");
+        copyright.getStyle().set("padding-left", "10px");
+
+        Button contactButton = new Button("Contact", e -> presenter.contact());
+        contactButton.getStyle().set("background-color", "#76d0f1");
+        contactButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+        contactButton.getStyle().set("color", "Black");
+
+        Button wikiButton = new Button("Wiki", e -> presenter.wiki());
+        wikiButton.getStyle().set("background-color", "#76d0f1");
+        wikiButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+        wikiButton.getStyle().set("color", "Black");
+
+        Button siteButton = new Button("Project Site", e -> presenter.projectSite());
+        siteButton.getStyle().set("background-color", "#76d0f1");
+        siteButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+        siteButton.getStyle().set("padding-right", "10px");
+        siteButton.getStyle().set("color", "Black");
+
+        footerHbox.add(copyright, contactButton, wikiButton, siteButton);
+        footerHbox.setDefaultVerticalComponentAlignment(Alignment.CENTER);
+        footerHbox.setSpacing(false);
+        footerHbox.getThemeList().add("spacing-xs");
+
+        footerHbox.getStyle().set("background-color", "#2596be");
+        footerHbox.setWidthFull();
         return footerHbox;
     }
 
-    public void setContent(Component content) {
-        this.content = content;
+    public void setContent(Component view) {
+        content.remove(oldContent);
+        content.add(view);
+        oldContent = view;
     }
 
     public void setPresenter(MainUIPresenter presenter) {
