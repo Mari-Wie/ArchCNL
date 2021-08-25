e595ee22bca6d3ad9d3535ade78d960cdb9b5ee8
cmkehler <christian.kehler@gmx.net>
2021-08-25 23:36:41 +0200

    Made main ui block a bit prettier

--- web-ui/src/main/java/org/vaadin/mainview/TestView2.java
+++ web-ui/src/main/java/org/vaadin/mainview/TestView2.java
@@ -4,18 +4,18 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
-public class TestView extends VerticalLayout {
+public class TestView2 extends VerticalLayout {
 
     private MainUIPresenter presenter;
 
-    public TestView(MainUIPresenter presenter) {
+    public TestView2(MainUIPresenter presenter) {
         this.presenter = presenter;
         initView();
     }
 
     private void initView() {
-        Label testText = new Label("This is the first view");
-        Button switchViewButton = new Button("Next", e -> presenter.setResultView());
+        Label testText = new Label("This is the second view");
+        Button switchViewButton = new Button("Back", e -> presenter.setArchitectureRuleView());
         add(testText, switchViewButton);
         setHeightFull();
     }
