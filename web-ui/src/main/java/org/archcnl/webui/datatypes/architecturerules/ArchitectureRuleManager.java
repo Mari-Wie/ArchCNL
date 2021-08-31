473e654daafb0264f8a8fc95d518daf2e66ee03d
Vincent Raudszus <vincent.raudszus@t-online.de>
2021-08-31 11:47:51 +0200

    ArchitectureRuleManager created

---
+++ web-ui/src/main/java/org/archcnl/webui/datatypes/architecturerules/ArchitectureRuleManager.java
@@ -0,0 +1,26 @@
+package org.archcnl.webui.datatypes.architecturerules;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArchitectureRuleManager {
+	
+	private List<ArchitectureRule> architectureRules;
+	
+	public ArchitectureRuleManager() {
+		architectureRules = new LinkedList<>();
+	}
+	
+	public void addArchitectureRule(ArchitectureRule architectureRule) {
+        architectureRules.add(architectureRule);
+    }
+	
+	public void deleteArchitectureRule(ArchitectureRule architectureRule) {
+        architectureRules.remove(architectureRule);
+    }
+	
+	public List<ArchitectureRule> getArchitectureRules() {
+        return architectureRules;
+    }
+
+}
