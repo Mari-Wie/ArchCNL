b47f7437055efd6358cd9bdb9a1e4c1fa90b2884
5engelha <5engelha@informatik.uni-hamburg.de>
2021-09-11 13:44:30 +0200

    Spotless apply

--- web-ui/src/main/java/org/archcnl/ui/input/ListEntryLayout.java
+++ web-ui/src/main/java/org/archcnl/ui/input/ListEntryLayout.java
@@ -1,37 +1,35 @@
 package org.archcnl.ui.input;
 
-
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
 public class ListEntryLayout<T> extends HorizontalLayout {
-	
+
     private static final long serialVersionUID = 2L;
-	
+
     private ListEntry<T> entry;
-	
-	private final String EDIT_TEXT = "Edit";
-	private final String DELETE_TEXT = "Delete";
-	private Button editButton;
-	private Button deleteButton;
-	
-	public ListEntryLayout(ListEntry<T> entry) {
-		this.entry = entry;
-		
-		Span text = new Span(entry.toString());
-		text.setWidth("100%");
-		addAndExpand(text);
-		if(entry.isLeaf()) {
-			editButton = new Button(EDIT_TEXT);
-			deleteButton = new Button(DELETE_TEXT);
-			add(editButton);
-			add(deleteButton);
-		}
-	}
 
-	public ListEntry<T> getListEntry() {
-		return entry;
-	}
+    private final String EDIT_TEXT = "Edit";
+    private final String DELETE_TEXT = "Delete";
+    private Button editButton;
+    private Button deleteButton;
 
+    public ListEntryLayout(ListEntry<T> entry) {
+        this.entry = entry;
+
+        Span text = new Span(entry.toString());
+        text.setWidth("100%");
+        addAndExpand(text);
+        if (entry.isLeaf()) {
+            editButton = new Button(EDIT_TEXT);
+            deleteButton = new Button(DELETE_TEXT);
+            add(editButton);
+            add(deleteButton);
+        }
+    }
+
+    public ListEntry<T> getListEntry() {
+        return entry;
+    }
 }
