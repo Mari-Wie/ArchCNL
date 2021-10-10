0b92c04075187436ea62e3ace2c132bfa01f3010
Vincent Raudszus <vincent.raudszus@t-online.de>
2021-10-10 18:24:40 +0200

    Revert "Revert "Readable QueryTextField""
    This reverts commit 75e77ece38821169b9256fc48811b98426a5fdcb.

--- web-ui/src/main/java/org/archcnl/ui/output/component/AbstractQueryResults.java
+++ web-ui/src/main/java/org/archcnl/ui/output/component/AbstractQueryResults.java
@@ -15,7 +15,23 @@ abstract class AbstractQueryResults extends VerticalLayout {
     protected Registration reg;
     protected GridView gridView;
     protected String exampleQuery =
-            "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> PREFIX conformance: <http://arch-ont.org/ontologies/architectureconformance#> PREFIX famix: <http://arch-ont.org/ontologies/famix.owl#> PREFIX architecture: <http://www.arch-ont.org/ontologies/architecture.owl#> SELECT ?cnl ?violation ?name WHERE { GRAPH ?g { ?rule rdf:type conformance:ArchitectureRule.  ?rule conformance:hasRuleRepresentation ?cnl.  ?aggregate rdf:type architecture:Aggregate.  ?aggregate famix:hasName ?name.  ?violation conformance:violates ?rule.  } }";
+            "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+                    + "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n"
+                    + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+                    + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n"
+                    + "PREFIX conformance: <http://arch-ont.org/ontologies/architectureconformance#>\n"
+                    + "PREFIX famix: <http://arch-ont.org/ontologies/famix.owl#>\n"
+                    + "PREFIX architecture: <http://www.arch-ont.org/ontologies/architecture.owl#>\n"
+                    + "\n"
+                    + "SELECT ?cnl ?violation ?name\n"
+                    + "WHERE { \n"
+                    + "GRAPH ?g { \n"
+                    + "\t?rule rdf:type conformance:ArchitectureRule. \n"
+                    + "\t?rule conformance:hasRuleRepresentation ?cnl. \n"
+                    + "\t?aggregate rdf:type architecture:Aggregate. \n"
+                    + "\t?aggregate famix:hasName ?name. \n"
+                    + "\t?violation conformance:violates ?rule. \n"
+                    + "} }";
     protected TextArea queryTextArea;
 
     public AbstractQueryResults() {
