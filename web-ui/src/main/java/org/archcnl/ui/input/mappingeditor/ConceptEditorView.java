2c3b3608eb727301811ddac3c1420a40b3847000
Vincent Raudszus <vincent.raudszus@t-online.de>
2021-10-15 17:29:10 +0200

    Exception changed

--- web-ui/src/main/java/org/archcnl/ui/input/mappingeditor/ConceptEditorView.java
+++ web-ui/src/main/java/org/archcnl/ui/input/mappingeditor/ConceptEditorView.java
@@ -3,7 +3,6 @@ package org.archcnl.ui.input.mappingeditor;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import java.util.Optional;
-import org.apache.commons.lang.NotImplementedException;
 import org.archcnl.domain.input.exceptions.InvalidVariableNameException;
 import org.archcnl.domain.input.model.mappings.ObjectType;
 import org.archcnl.domain.input.model.mappings.Variable;
@@ -53,7 +52,7 @@ public class ConceptEditorView extends MappingEditorView {
 
     @Override
     public Optional<ObjectType> getThenTripletObject() {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 
     @Override
