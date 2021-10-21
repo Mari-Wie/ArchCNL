3cf785ceb12df10c1806485e842ef85edd550630
cmkehler <christian.kehler@gmx.net>
2021-10-21 20:46:08 +0200

    removed saveRule method from the contract

--- web-ui/src/main/java/org/archcnl/ui/input/ruleeditor/ArchitectureRulesContract.java
+++ web-ui/src/main/java/org/archcnl/ui/input/ruleeditor/ArchitectureRulesContract.java
@@ -4,9 +4,7 @@ import java.io.Serializable;
 
 public interface ArchitectureRulesContract {
 
-    public interface View {
-        public void saveRule();
-    }
+    public interface View {}
 
     public interface Presenter<T extends View> extends Serializable {
         public void saveArchitectureRule(String potentialRule);
