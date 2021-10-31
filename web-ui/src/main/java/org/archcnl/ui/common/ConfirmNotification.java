e8d861dd91d73debc4621adfab96f8f338d2fbf0
Vincent Raudszus <vincent.raudszus@t-online.de>
2021-10-31 20:28:48 +0100

    Notification centered and resized

--- web-ui/src/main/java/org/archcnl/ui/common/ConfirmNotification.java
+++ web-ui/src/main/java/org/archcnl/ui/common/ConfirmNotification.java
@@ -1,6 +1,7 @@
 package org.archcnl.ui.common;
 
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
@@ -12,6 +13,7 @@ public class ConfirmNotification extends Notification {
     private static final long serialVersionUID = -7499231869322025697L;
 
     public ConfirmNotification(String message) {
+        setPosition(Position.MIDDLE);
         VerticalLayout vl = new VerticalLayout();
         vl.add(new Text(message));
         HorizontalLayout buttonRow = new HorizontalLayout();
@@ -20,5 +22,7 @@ public class ConfirmNotification extends Notification {
         buttonRow.add(new Button("OK", click -> close()));
         vl.add(buttonRow);
         add(vl);
+        vl.setMinHeight(7.0f, Unit.CM);
+        vl.setMinWidth(12.0f, Unit.CM);
     }
 }
