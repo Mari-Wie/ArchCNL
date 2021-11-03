61a29af1cf48e621a685703b61688f15dc92151a
Vincent Raudszus <vincent.raudszus@t-online.de>
2021-11-03 13:39:00 +0100

    OkCancelNotification changed to Dialog

--- web-ui/src/main/java/org/archcnl/ui/common/OkCancelDialog.java
+++ web-ui/src/main/java/org/archcnl/ui/common/OkCancelDialog.java
@@ -3,25 +3,28 @@ package org.archcnl.ui.common;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
-public class OkCancelNotification extends Notification {
+public class OkCancelDialog extends Dialog {
 
     private static final long serialVersionUID = 8174115191925312064L;
 
-    public OkCancelNotification(
+    public OkCancelDialog(
             String headline,
             String body,
             ButtonClickResponder okResponse,
             ButtonClickResponder cancelResponse) {
-        VerticalLayout vl = new VerticalLayout();
-        VerticalLayout textLayout = new VerticalLayout(new H3(headline), new Text(body));
-        vl.add(textLayout);
-        vl.setFlexGrow(1, textLayout);
+
+        setDraggable(true);
+        setMinHeight(8.0f, Unit.CM);
+        setWidth(12.0f, Unit.CM);
+
+        Text headlineText = new Text(headline);
+        VerticalLayout bodyText = new VerticalLayout(new Text(body));
+        bodyText.setWidthFull();
         HorizontalLayout buttonRow = new HorizontalLayout();
         buttonRow.setWidthFull();
         buttonRow.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
@@ -39,10 +42,10 @@ public class OkCancelNotification extends Notification {
                             cancelResponse.onButtonClick();
                             close();
                         }));
-        vl.add(buttonRow);
-        add(vl);
-        vl.setMinHeight(7.0f, Unit.CM);
-        vl.setMinWidth(12.0f, Unit.CM);
-        setPosition(Position.MIDDLE);
+
+        VerticalLayout layout = new VerticalLayout(headlineText, bodyText, buttonRow);
+        layout.setHeightFull();
+        layout.setFlexGrow(1, bodyText);
+        add(layout);
     }
 }
