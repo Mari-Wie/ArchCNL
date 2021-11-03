6b0abd29e5b55c9fbd05fc6fec7c82f4e1ced812
Vincent Raudszus <vincent.raudszus@t-online.de>
2021-11-03 13:13:33 +0100

    ConfirmNotification changed to Dialog

--- web-ui/src/main/java/org/archcnl/ui/common/ConfirmDialog.java
+++ web-ui/src/main/java/org/archcnl/ui/common/ConfirmDialog.java
@@ -3,17 +3,16 @@ package org.archcnl.ui.common;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
-public class ConfirmNotification extends Notification {
+public class ConfirmDialog extends Dialog {
 
     private static final long serialVersionUID = -7499231869322025697L;
 
-    public ConfirmNotification(String message) {
-        setPosition(Position.MIDDLE);
+    public ConfirmDialog(String message) {
         VerticalLayout vl = new VerticalLayout();
         vl.add(new Text(message));
         HorizontalLayout buttonRow = new HorizontalLayout();
