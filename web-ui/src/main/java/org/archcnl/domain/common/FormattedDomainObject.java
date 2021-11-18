db33471ea80fe61024c915945d9b508ed82c4651
AlexHamburg <oleksandr.trunov@gmail.com>
2021-11-18 19:08:17 +0100

    ARCHCNL-104: Add fromatted interfaces for domain objects

--- web-ui/src/main/java/org/archcnl/domain/common/FormattedDomainObject.java
+++ web-ui/src/main/java/org/archcnl/domain/common/FormattedDomainObject.java
@@ -2,6 +2,4 @@ package org.archcnl.domain.common;
 
 /** Domain object that has pretty representation for query to Stardog, ADOC files and GUI. */
 public interface FormattedDomainObject
-        extends FormattedQueryDomainObject, FormattedViewDomainObject, FormattedAdocDomainObject {
-
-}
+        extends FormattedQueryDomainObject, FormattedViewDomainObject, FormattedAdocDomainObject {}
