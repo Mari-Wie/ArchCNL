3f8fdae96b9c46ecf04782d695fa3b005d90ec22
Vincent Raudszus <vincent.raudszus@t-online.de>
2021-11-26 09:33:57 +0100

    duplicated code removed

--- web-ui/src/main/java/org/archcnl/domain/common/StringValue.java
+++ web-ui/src/main/java/org/archcnl/domain/common/StringValue.java
@@ -51,33 +51,4 @@ public class StringValue extends ObjectType {
     protected int requiredHashCodeOverride() {
         return Objects.hash(value);
     }
-
-    @Override
-    public String transformToSparqlQuery() {
-        return "\"" + value + "\"" + "^^xsd:string";
-    }
-
-    @Override
-    public String transformToGui() {
-        return transformToAdoc();
-    }
-
-    @Override
-    public String transformToAdoc() {
-        return "'" + value + "'";
-    }
-
-    @Override
-    protected boolean requiredEqualsOverride(Object obj) {
-        if (obj instanceof StringValue) {
-            final StringValue that = (StringValue) obj;
-            return Objects.equals(this.getValue(), that.getValue());
-        }
-        return false;
-    }
-
-    @Override
-    protected int requiredHashCodeOverride() {
-        return Objects.hash(value);
-    }
 }
