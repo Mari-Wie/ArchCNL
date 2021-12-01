9d77246d7ff36a42dd896f505bef08b79a36ec56
5engelha <5engelha@informatik.uni-hamburg.de>
2021-12-01 23:09:17 +0100

    Add bar with buttons

--- web-ui/src/main/java/org/archcnl/ui/output/component/FreeTextQueryUiComponent.java
+++ web-ui/src/main/java/org/archcnl/ui/output/component/FreeTextQueryUiComponent.java
@@ -1,20 +1,28 @@
 package org.archcnl.ui.output.component;
 
+import org.archcnl.ui.output.events.ResultUpdateEvent;
+
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
 public class FreeTextQueryUiComponent extends AbstractQueryResults {
 
     private static final long serialVersionUID = 1L;
 
-    HorizontalLayout buttonBar;
+    private HorizontalLayout buttonBar;
+    private Button clearButton = new Button("Clear", e -> queryTextArea.clear());
+    private Button defaultQueryButton = new Button("Default Query", e -> queryTextArea.setValue(exampleQuery));
+    private Button importCustomQueryButton = new Button("Import Custom Query", e -> importCustomQueryText());
+    private Button applyButton = new Button("Apply", e -> fireEvent(new ResultUpdateEvent(this, false)));
 
     public FreeTextQueryUiComponent() {
         registerEventListeners();
+        buttonBar = new HorizontalLayout(clearButton, defaultQueryButton, importCustomQueryButton, applyButton);
         addComponents();
     }
 
     protected void addComponents() {
-        add(queryTextArea, gridView);
+        add(queryTextArea, buttonBar, gridView);
     }
     
     private void importCustomQueryText() {
