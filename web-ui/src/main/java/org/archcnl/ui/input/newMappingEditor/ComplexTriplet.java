89728b27fa72e4aee75aa810a3974e2bf2cf5688
Oliver Heidmann <oliver.heidmann@mpimet.mpg.de>
2021-12-04 15:34:09 +0000

    added variable type selection

--- web-ui/src/main/java/org/archcnl/ui/input/newMappingEditor/ComplexTriplet.java
+++ web-ui/src/main/java/org/archcnl/ui/input/newMappingEditor/ComplexTriplet.java
@@ -1,12 +1,14 @@
 package org.archcnl.ui.input.newMappingEditor;
 
+import com.vaadin.flow.component.AbstractField.ComponentValueChangeEvent;
 import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.shared.Registration;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,51 +23,88 @@ public class ComplexTriplet extends HorizontalLayout {
     ComboBox<String> subjectComboBox;
     ComboBox<String> predicateComboBox;
     ComboBox<String> variableTypeComboBox;
-    ComboBox<String> objectComboBox;
+    Component object;
     AddRemoveButtonLayout addRemoveButtonLayout;
 
     public ComplexTriplet() {
         createComboBoxes();
         createAddRemoveButtons();
     }
-    
-    List<String> gather(){
-    	return new ArrayList<String>();
+
+    List<String> gather() {
+        return new ArrayList<String>();
     }
 
-    private void createComboBoxes() {
+    void createSubjectComboBox() {
         subjectComboBox = new ComboBox<String>();
         subjectComboBox.setAllowCustomValue(true);
         subjectComboBox.addCustomValueSetListener(
                 e -> fireEvent(new VariableSelectionEvent(subjectComboBox, false, e.getDetail())));
+        subjectComboBox.addFocusListener(
+                e -> fireEvent(new VariableListUpdateRequest(subjectComboBox, false)));
         add(subjectComboBox);
+    }
+
+    void createPredicateComboBox() {
 
         predicateComboBox = new ComboBox<String>();
         predicateComboBox.setAllowCustomValue(false);
         predicateComboBox.addValueChangeListener(
-                e ->{
-                        fireEvent(
-                                new PredicateSelectionEvent(
-                                        predicateComboBox,
-                                        variableTypeComboBox,
-                                        false,
-                                        e.getValue()));
-    });
+                e -> {
+                    fireEvent(
+                            new PredicateSelectionEvent(
+                                    predicateComboBox, variableTypeComboBox, false, e.getValue()));
+                });
         add(predicateComboBox);
+    }
+
+    void handleVariableTypeSelectionEvent(ComponentValueChangeEvent<ComboBox<String>, String> e) {
+        String val = e.getValue();
+        System.out.println("handling variable type selection for " + val);
+        Component newComp = null;
+        if (val == "Variable") {
+            System.out.println("handling variable type Variable");
+            ComboBox<String> newBox = new ComboBox<String>();
+            newBox.setPlaceholder("Select Variable");
+            fireEvent(new VariableListUpdateRequest(newBox, false));
+            newComp = newBox;
+        } else if (val == "Boolean") {
+            System.out.println("handling variable type Boolean");
+            ComboBox<String> newBox = new ComboBox<String>();
+            newBox.setItems(Arrays.asList("True", "False"));
+            newBox.setValue("True");
+            newComp = newBox;
+        } else if (val == "String") {
+            System.out.println("handling variable type String");
+            TextField textField = new TextField();
+            textField.setPlaceholder("Custom String");
+            newComp = textField;
+        }
+        if (newComp != null) {
+            addComponentAtIndex(indexOf(object), newComp);
+            remove(object);
+            object = newComp;
+        }
+    }
+
+    private void createComboBoxes() {
+
+        createSubjectComboBox();
+        createPredicateComboBox();
 
         variableTypeComboBox = new ComboBox<String>();
+        variableTypeComboBox.addValueChangeListener(e -> handleVariableTypeSelectionEvent(e));
         add(variableTypeComboBox);
 
-        objectComboBox = new ComboBox<String>();
-        add(objectComboBox);
+        object = new ComboBox<String>();
+        add(object);
     }
-    
+
     @Override
-	protected
-    void onAttach(AttachEvent attachEvent) {
-    	System.out.println("Attach thing was called");
+    protected void onAttach(AttachEvent attachEvent) {
+        System.out.println("Attach thing was called");
         updateVariablePossibilities();
-		updatePredicatePossibilities();
+        updatePredicatePossibilities();
     }
 
     private void createAddRemoveButtons() {
@@ -85,8 +124,10 @@ public class ComplexTriplet extends HorizontalLayout {
     }
 
     private void updatePredicatePossibilities() {
-    	System.out.println("Init event for predicates");
-        List<String> testItems = Arrays.asList("not initialized", "fix this", "whoever reads this knows our reviews suck");
+        System.out.println("Init event for predicates");
+        List<String> testItems =
+                Arrays.asList(
+                        "not initialized", "fix this", "whoever reads this knows our reviews suck");
         predicateComboBox.setItems(testItems);
         fireEvent(new PredicateListUpdateRequest(predicateComboBox, false));
     }
