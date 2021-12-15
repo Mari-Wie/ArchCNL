e7f8f5624d53c591b6a07a767a5a9d4ae6a16012
Vincent Raudszus <vincent.raudszus@t-online.de>
2021-12-15 19:04:25 +0100

    TripletView fixed

--- web-ui/src/main/java/org/archcnl/ui/input/mappingeditor/triplet/TripletView.java
+++ web-ui/src/main/java/org/archcnl/ui/input/mappingeditor/triplet/TripletView.java
@@ -7,106 +7,61 @@ import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.shared.Registration;
-import org.archcnl.domain.common.ObjectType;
-import org.archcnl.domain.common.Relation;
-import org.archcnl.domain.common.Triplet;
-import org.archcnl.domain.common.Variable;
-import org.archcnl.domain.common.VariableManager;
 import org.archcnl.ui.input.mappingeditor.events.AddTripletViewAfterButtonPressedEvent;
+import org.archcnl.ui.input.mappingeditor.events.ConceptListUpdateRequestedEvent;
+import org.archcnl.ui.input.mappingeditor.events.ConceptSelectedEvent;
 import org.archcnl.ui.input.mappingeditor.events.PredicateSelectedEvent;
+import org.archcnl.ui.input.mappingeditor.events.RelationListUpdateRequestedEvent;
 import org.archcnl.ui.input.mappingeditor.events.TripletViewDeleteButtonPressedEvent;
+import org.archcnl.ui.input.mappingeditor.events.VariableCreationRequestedEvent;
+import org.archcnl.ui.input.mappingeditor.events.VariableFilterChangedEvent;
 import org.archcnl.ui.input.mappingeditor.events.VariableListUpdateRequestedEvent;
-import org.archcnl.ui.input.mappingeditor.events.VariableSelectedEvent;
 
 public class TripletView extends HorizontalLayout {
 
     private static final long serialVersionUID = -547117976123681486L;
 
-    HorizontalLayout tripletLayout = new HorizontalLayout();
     private VariableSelectionComponent subjectComponent;
     private PredicateComponent predicateComponent;
     private ObjectView objectView;
 
-    private void initLayoutFormat() {
+    public TripletView() {
         setPadding(false);
         setDefaultVerticalComponentAlignment(Alignment.BASELINE);
         setWidthFull();
+
+        subjectComponent = new VariableSelectionComponent();
+        predicateComponent = new PredicateComponent();
+        objectView = new ObjectView();
+
+        HorizontalLayout tripletLayout = new HorizontalLayout();
+        tripletLayout.setWidthFull();
+        tripletLayout.add(subjectComponent);
+        tripletLayout.add(predicateComponent);
+        tripletLayout.add(objectView);
+        add(tripletLayout);
+
+        addCreateRemoveButtons();
+
+        addListeners();
     }
 
     private void addListeners() {
-        subjectComponent.addListener(
-                VariableSelectedEvent.class,
-                e -> {
-                    System.out.println("refired VariableSelectedEvent in TripletView");
-                    fireEvent(e);
-                });
-        subjectComponent.addListener(
-                VariableListUpdateRequestedEvent.class,
-                e -> {
-                    System.out.println("refired VariableListUpdateRequestedEvent in TripletView");
-                    fireEvent(e);
-                });
-        subjectComponent.addListener(
-                PredicateSelectedEvent.class,
-                e -> {
-                    System.out.println("refired PredicateSelectedEvent in TripletView");
-                    fireEvent(e);
-                });
+        subjectComponent.addListener(VariableFilterChangedEvent.class, this::fireEvent);
+        subjectComponent.addListener(VariableCreationRequestedEvent.class, this::fireEvent);
+        subjectComponent.addListener(VariableListUpdateRequestedEvent.class, this::fireEvent);
+
+        predicateComponent.addListener(PredicateSelectedEvent.class, this::fireEvent);
+        predicateComponent.addListener(RelationListUpdateRequestedEvent.class, this::fireEvent);
+
+        objectView.addListener(VariableFilterChangedEvent.class, this::fireEvent);
+        objectView.addListener(VariableCreationRequestedEvent.class, this::fireEvent);
+        objectView.addListener(VariableListUpdateRequestedEvent.class, this::fireEvent);
+        objectView.addListener(ConceptListUpdateRequestedEvent.class, this::fireEvent);
+        objectView.addListener(ConceptSelectedEvent.class, this::fireEvent);
     }
 
-    public TripletView(
-            TripletPresenter presenter, VariableManager variableManager, Triplet triplet) {
-        initLayoutFormat();
-
-        createTripletLayout(triplet.getSubject(), triplet.getPredicate(), triplet.getObject());
-        createCreateRemoveManagementComponent();
-        addListeners();
-    }
-
-    public TripletView(TripletPresenter presenter, VariableManager variableManager) {
-        initLayoutFormat();
-        createTripletLayout();
-        createCreateRemoveManagementComponent();
-        addListeners();
-    }
-
-    // TODO fix code duplication and look for way of default init
-    private void createTripletLayout() {
-
-        tripletLayout.setWidthFull();
-
-        createSubjectComponent("Subject");
-        createPredicateComponent("Predicate");
-        createObjectView("Object");
-        add(tripletLayout);
-    }
-
-    private void createTripletLayout(Variable subject, Relation relation, ObjectType object) {
-        tripletLayout.setWidthFull();
-
-        createSubjectComponent(subject.getName());
-        createPredicateComponent(relation.getName());
-        createObjectView(object.getName());
-        add(tripletLayout);
-    }
-
-    private void createSubjectComponent(String initValue) {
-        subjectComponent = new SubjectComponent(null); // TODO there once was a var manager
-        tripletLayout.add(subjectComponent);
-    }
-
-    private void createPredicateComponent(String initValue) {
-        predicateComponent = new PredicateComponent(objectView);
-        predicateComponent.addListener(PredicateSelectedEvent.class, e -> fireEvent(e));
-        tripletLayout.add(predicateComponent);
-    }
-
-    private void createObjectView(String initValue) {
-        objectView = new ObjectView(null); // TODO there once was a var manager
-        tripletLayout.add(objectView);
-    }
-
-    private void createCreateRemoveManagementComponent() {
+    private void addCreateRemoveButtons() {
         add(
                 new Button(
                         new Icon(VaadinIcon.TRASH),
