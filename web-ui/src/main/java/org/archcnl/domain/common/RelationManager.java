593e74a0b65464215ab5e8acb481a002d4e2121b
Oleksandr Trunov <oleksandr.trunov@gmail.com>
2022-01-23 12:16:29 +0100

    Feat/archcnl 142/add description to relations (#261)
    * ARCHCNL-142: Add descriptions for relations in the RelationsManager
    
    * ARCHCNL-142: Run spotless apply
    
    * ARCHCNL-142: Run spotless apply

--- web-ui/src/main/java/org/archcnl/domain/common/RelationManager.java
+++ web-ui/src/main/java/org/archcnl/domain/common/RelationManager.java
@@ -151,9 +151,21 @@ public class RelationManager {
     private void initializeBoolRelations() {
         final List<ActualObjectType> boolConcept = new LinkedList<>();
         boolConcept.add(new BooleanValue(false));
-        relations.add(new FamixRelation("isConstructor", "", boolConcept));
-        relations.add(new FamixRelation("isExternal", "", boolConcept));
-        relations.add(new FamixRelation("isInterface", "", boolConcept));
+        relations.add(
+                new FamixRelation(
+                        "isConstructor",
+                        "This relation is used to state that the subject is or isn't a constructor (based on a value in the object). A constructor is special method that is called when an object is instantiated.",
+                        boolConcept));
+        relations.add(
+                new FamixRelation(
+                        "isExternal",
+                        "This relation is used to state that the subject is or isn't an external type (based on a value in the object).",
+                        boolConcept));
+        relations.add(
+                new FamixRelation(
+                        "isInterface",
+                        "This relation is used to state that the subject is or isn't an interface (based on a value in the object). An interface is an abstract type that is used to specify a behavior that classes must implement.",
+                        boolConcept));
     }
 
     private void initializeObjectRelations(final ConceptManager conceptManager)
@@ -164,32 +176,72 @@ public class RelationManager {
                 conceptManager
                         .getConceptByName("FamixClass")
                         .orElseThrow(() -> new ConceptDoesNotExistException("FamixClass")));
-        relations.add(new FamixRelation("hasDefiningClass", "", famixClassConcept));
-        relations.add(new FamixRelation("hasDeclaredException", "", famixClassConcept));
-        relations.add(new FamixRelation("hasCaughtException", "", famixClassConcept));
-        relations.add(new FamixRelation("throwsException", "", famixClassConcept));
-        relations.add(new FamixRelation("hasSubClass", "", famixClassConcept));
-        relations.add(new FamixRelation("hasSuperClass", "", famixClassConcept));
+        relations.add(
+                new FamixRelation(
+                        "hasDefiningClass",
+                        "This relation is used to state that the subject has or is a specified in the object class or type.",
+                        famixClassConcept));
+        relations.add(
+                new FamixRelation(
+                        "hasDeclaredException",
+                        "This relation is used to state that the subject (for example a method) has a specified in the object exception type.",
+                        famixClassConcept));
+        relations.add(
+                new FamixRelation(
+                        "hasCaughtException",
+                        "This relation is used to state that the subject (for example a method) catches a specified in the object exception type.",
+                        famixClassConcept));
+        relations.add(
+                new FamixRelation(
+                        "throwsException",
+                        "This relation is used to state that the subject (for example a method) throws a specified in the object exception type.",
+                        famixClassConcept));
+        relations.add(
+                new FamixRelation(
+                        "hasSubClass",
+                        "This relation is used to state that the subject has a sub class of the specified in the object type.",
+                        famixClassConcept));
+        relations.add(
+                new FamixRelation(
+                        "hasSuperClass",
+                        "This relation is used to state that the subject extends a super class of the specified in the object type.",
+                        famixClassConcept));
 
         // Parameter relations
         final List<ActualObjectType> parameterConcept = new LinkedList<>();
         parameterConcept.add(extractConcept(conceptManager, "Parameter"));
-        relations.add(new FamixRelation("definesParameter", "", parameterConcept));
+        relations.add(
+                new FamixRelation(
+                        "definesParameter",
+                        "This relation is used to state that the subject (for example a method) has specified in the object parameters.",
+                        parameterConcept));
 
         // LocalVariable relations
         final List<ActualObjectType> localVariableConcept = new LinkedList<>();
         localVariableConcept.add(extractConcept(conceptManager, "LocalVariable"));
-        relations.add(new FamixRelation("definesVariable", "", localVariableConcept));
+        relations.add(
+                new FamixRelation(
+                        "definesVariable",
+                        "This relation is used to state that the subject (for example a method) defines a specified in the object variable.",
+                        localVariableConcept));
 
         // AnnotationInstance relations
         final List<ActualObjectType> annotationInstanceConcept = new LinkedList<>();
         annotationInstanceConcept.add(extractConcept(conceptManager, "AnnotationInstance"));
-        relations.add(new FamixRelation("hasAnnotationInstance", "", annotationInstanceConcept));
+        relations.add(
+                new FamixRelation(
+                        "hasAnnotationInstance",
+                        "This relation is used to state that the subject has a specified in the object annotation.",
+                        annotationInstanceConcept));
 
         // AnnotationType relations
         final List<ActualObjectType> annotationTypeConcept = new LinkedList<>();
         annotationTypeConcept.add(extractConcept(conceptManager, "AnnotationType"));
-        relations.add(new FamixRelation("hasAnnotationType", "", annotationTypeConcept));
+        relations.add(
+                new FamixRelation(
+                        "hasAnnotationType",
+                        "This relation is used to state that the subject has a specified in the object annotations type.",
+                        annotationTypeConcept));
 
         // AnnotationTypeAttribute relations
         final List<ActualObjectType> annotationTypeAttributeConcept = new LinkedList<>();
@@ -197,7 +249,9 @@ public class RelationManager {
                 extractConcept(conceptManager, "AnnotationTypeAttribute"));
         relations.add(
                 new FamixRelation(
-                        "hasAnnotationTypeAttribute", "", annotationTypeAttributeConcept));
+                        "hasAnnotationTypeAttribute",
+                        "This relation is used to state that the subject has in the annotation specified in the object attributes of an annotation type.",
+                        annotationTypeAttributeConcept));
 
         // AnnotationInstanceAttribute relations
         final List<ActualObjectType> annotationInstanceAttributeConcept = new LinkedList<>();
@@ -205,30 +259,48 @@ public class RelationManager {
                 extractConcept(conceptManager, "AnnotationInstanceAttribute"));
         relations.add(
                 new FamixRelation(
-                        "hasAnnotationInstanceAttribute", "", annotationInstanceAttributeConcept));
+                        "hasAnnotationInstanceAttribute",
+                        "This relation is used to state that the subject has in the annotation specified in the object an attribute-value pair.",
+                        annotationInstanceAttributeConcept));
 
         // Attribute relations
         final List<ActualObjectType> attributeConcept = new LinkedList<>();
         attributeConcept.add(extractConcept(conceptManager, "Attribute"));
-        relations.add(new FamixRelation("definesAttribute", "", attributeConcept));
+        relations.add(
+                new FamixRelation(
+                        "definesAttribute",
+                        "This relation is used to state that the subject (for example a class) defines specified in the object attribute.",
+                        attributeConcept));
 
         // Method relations
         final List<ActualObjectType> methodConcept = new LinkedList<>();
         methodConcept.add(extractConcept(conceptManager, "Method"));
-        relations.add(new FamixRelation("definesMethod", "", methodConcept));
+        relations.add(
+                new FamixRelation(
+                        "definesMethod",
+                        "This relation is used to state that the subject (for example a class) defines specified in the object method.",
+                        methodConcept));
 
         // Type relations
         final List<ActualObjectType> typeConcepts = new LinkedList<>();
         typeConcepts.add(extractConcept(conceptManager, "FamixClass"));
         typeConcepts.add(extractConcept(conceptManager, "Enum"));
         typeConcepts.add(extractConcept(conceptManager, "AnnotationType"));
-        relations.add(new FamixRelation("imports", "", typeConcepts));
+        relations.add(
+                new FamixRelation(
+                        "imports",
+                        "This relation is used to state that the subject (for example a class) imports (has dependency to) specified in the object type, class etc.",
+                        typeConcepts));
 
         // Class and Enum relations
         final List<ActualObjectType> classEnumConcepts = new LinkedList<>();
         classEnumConcepts.add(extractConcept(conceptManager, "FamixClass"));
         classEnumConcepts.add(extractConcept(conceptManager, "Enum"));
-        relations.add(new FamixRelation("definesNestedType", "", classEnumConcepts));
+        relations.add(
+                new FamixRelation(
+                        "definesNestedType",
+                        "This relation is used to state that the subject (for example a class) defines a nested specified in the object type.",
+                        classEnumConcepts));
 
         // Type + NameSpace relations
         final List<ActualObjectType> namespaceContainsConcepts = new LinkedList<>();
@@ -236,7 +308,11 @@ public class RelationManager {
         namespaceContainsConcepts.add(extractConcept(conceptManager, "FamixClass"));
         namespaceContainsConcepts.add(extractConcept(conceptManager, "Enum"));
         namespaceContainsConcepts.add(extractConcept(conceptManager, "AnnotationType"));
-        relations.add(new FamixRelation("namespaceContains", "", namespaceContainsConcepts));
+        relations.add(
+                new FamixRelation(
+                        "namespaceContains",
+                        "This relation is used to state that the subject (namespace) contains a specified in the object type, class etc.",
+                        namespaceContainsConcepts));
 
         // Type + Primitive relations
         final List<ActualObjectType> typesAndprimitives = new LinkedList<>();
@@ -245,7 +321,11 @@ public class RelationManager {
         typesAndprimitives.add(extractConcept(conceptManager, "AnnotationType"));
         typesAndprimitives.add(new StringValue(""));
         typesAndprimitives.add(new BooleanValue(false));
-        relations.add(new FamixRelation("hasDeclaredType", "", typesAndprimitives));
+        relations.add(
+                new FamixRelation(
+                        "hasDeclaredType",
+                        "This relation is used to state that the subject (for example attribute) has a specified in the object type.",
+                        typesAndprimitives));
     }
 
     private void initializeConformanceRelations(final ConceptManager conceptManager)
@@ -256,29 +336,31 @@ public class RelationManager {
         relations.add(
                 new ConformanceRelation(
                         "hasRuleRepresentation",
-                        "",
+                        "This relation is used to state that the subject (for example architecture rule) has a specified in the object representation (in most cases string representation).",
                         new ArrayList<>(Arrays.asList(new StringValue("")))));
         relations.add(
                 new ConformanceRelation(
-                        "hasRuleType", "", new ArrayList<>(Arrays.asList(new StringValue("")))));
+                        "hasRuleType",
+                        "This relation is used to state that the subject (for example architecture rule) has a specified in the object rule type.",
+                        new ArrayList<>(Arrays.asList(new StringValue("")))));
         relations.add(
                 new ConformanceRelation(
                         "hasNotInferredStatement",
-                        "",
+                        "This relation is used to state that the subject hasn't / doesn't correspond to the specified in the object statements.",
                         new ArrayList<>(
                                 Arrays.asList(
                                         extractConcept(conceptManager, "NotInferredStatement")))));
         relations.add(
                 new ConformanceRelation(
                         "hasAssertedStatement",
-                        "",
+                        "This relation is used to state that the subject has / corresponds to the specified in the object statements.",
                         new ArrayList<>(
                                 Arrays.asList(
                                         extractConcept(conceptManager, "AssertedStatement")))));
         relations.add(
                 new ConformanceRelation(
                         "hasSubject",
-                        "",
+                        "This relation is used to state that the subject (for example statement) has a specified subject.",
                         new ArrayList<>(
                                 Arrays.asList(
                                         extractConcept(conceptManager, "NotInferredStatement"),
@@ -286,7 +368,7 @@ public class RelationManager {
         relations.add(
                 new ConformanceRelation(
                         "hasPredicate",
-                        "",
+                        "This relation is used to state that the subject (for example statement) has a specified predicate.",
                         new ArrayList<>(
                                 Arrays.asList(
                                         extractConcept(conceptManager, "NotInferredStatement"),
@@ -294,7 +376,7 @@ public class RelationManager {
         relations.add(
                 new ConformanceRelation(
                         "hasObject",
-                        "",
+                        "This relation is used to state that the subject (for example statement) has a specified object.",
                         new ArrayList<>(
                                 Arrays.asList(
                                         extractConcept(conceptManager, "NotInferredStatement"),
@@ -302,35 +384,35 @@ public class RelationManager {
         relations.add(
                 new ConformanceRelation(
                         "proofs",
-                        "",
+                        "This relation is used to state that the subject (proof) verifies a specified in the object violation.",
                         new ArrayList<>(
                                 Arrays.asList(
                                         extractConcept(conceptManager, "ArchitectureViolation")))));
         relations.add(
                 new ConformanceRelation(
                         "hasDetected",
-                        "",
+                        "This relation is used to state that the subject (conformance check) has detected a specified in the object violation.",
                         new ArrayList<>(
                                 Arrays.asList(
                                         extractConcept(conceptManager, "ArchitectureViolation")))));
         relations.add(
                 new ConformanceRelation(
                         "hasViolation",
-                        "",
+                        "This relation is used to state that the subject has a specified in the object violation.",
                         new ArrayList<>(
                                 Arrays.asList(
                                         extractConcept(conceptManager, "ArchitectureViolation")))));
         relations.add(
                 new ConformanceRelation(
                         "violates",
-                        "",
+                        "This relation is used to state that the subject violates a specified in the object rule.",
                         new ArrayList<>(
                                 Arrays.asList(
                                         extractConcept(conceptManager, "ArchitectureRule")))));
         relations.add(
                 new ConformanceRelation(
                         "validates",
-                        "",
+                        "This relation is used to state that the subject validates a specified in the object rule or conformance check.",
                         new ArrayList<>(
                                 Arrays.asList(
                                         extractConcept(conceptManager, "ArchitectureRule")))));
