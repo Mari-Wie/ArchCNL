ce1b983adf78a744dd36a695c15787647d480590
Vincent Raudszus <vincent.raudszus@t-online.de>
2022-01-25 20:11:12 +0100

    AdocEndToEndTest expanded and simplified

--- web-ui/src/test/java/org/archcnl/domain/common/io/AdocEndToEndTest.java
+++ web-ui/src/test/java/org/archcnl/domain/common/io/AdocEndToEndTest.java
@@ -1,7 +1,6 @@
 package org.archcnl.domain.common.io;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,11 +9,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import org.apache.commons.io.FileUtils;
-import org.archcnl.domain.TestUtils;
 import org.archcnl.domain.common.ConceptManager;
 import org.archcnl.domain.common.RelationManager;
-import org.archcnl.domain.common.io.importhelper.MappingExtractor;
-import org.archcnl.domain.common.io.importhelper.RuleExtractor;
 import org.archcnl.domain.input.exceptions.ConceptDoesNotExistException;
 import org.archcnl.domain.input.exceptions.RelationDoesNotExistException;
 import org.archcnl.domain.input.model.architecturerules.ArchitectureRuleManager;
@@ -71,32 +67,6 @@ class AdocEndToEndTest {
         String expectedContent = FileUtils.readFileToString(ruleFile, StandardCharsets.UTF_8);
         String actualContent = FileUtils.readFileToString(writtenFile, StandardCharsets.UTF_8);
 
-        assertEquals(
-                TestUtils.numberOfMatches(RuleExtractor.getRuleContentPattern(), expectedContent),
-                TestUtils.numberOfMatches(RuleExtractor.getRuleContentPattern(), actualContent));
-        assertEquals(
-                TestUtils.numberOfMatches(
-                        MappingExtractor.getConceptMappingPattern(), expectedContent),
-                TestUtils.numberOfMatches(
-                        MappingExtractor.getConceptMappingPattern(), actualContent));
-        assertEquals(
-                TestUtils.numberOfMatches(
-                        MappingExtractor.getRelationMappingPattern(), expectedContent),
-                TestUtils.numberOfMatches(
-                        MappingExtractor.getRelationMappingPattern(), actualContent));
-
-        assertTrue(
-                TestUtils.doAllMatchesExistInSecondString(
-                        RuleExtractor.getRuleContentPattern(), expectedContent, actualContent));
-        assertTrue(
-                TestUtils.doAllMatchesExistInSecondString(
-                        MappingExtractor.getConceptMappingPattern(),
-                        expectedContent,
-                        actualContent));
-        assertTrue(
-                TestUtils.doAllMatchesExistInSecondString(
-                        MappingExtractor.getRelationMappingPattern(),
-                        expectedContent,
-                        actualContent));
+        assertEquals(expectedContent, actualContent);
     }
 }
