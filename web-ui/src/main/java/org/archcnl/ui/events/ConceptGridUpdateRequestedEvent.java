4ff4863a323fa01596916426e17e75e221e28397
Oliver Heidmann <oliver.heidmann@mpimet.mpg.de>
2022-01-27 17:48:34 +0100

    edded Hierarchy update events

---
+++ web-ui/src/main/java/org/archcnl/ui/events/ConceptGridUpdateRequestedEvent.java
@@ -0,0 +1,14 @@
+package org.archcnl.ui.events;
+
+import com.vaadin.flow.component.ComponentEvent;
+import org.archcnl.ui.common.conceptandrelationlistview.HierarchyView;
+import org.archcnl.domain.common.Concept;
+
+public class ConceptGridUpdateRequestedEvent extends ComponentEvent<HierarchyView<Concept>> {
+
+    private static final long serialVersionUID = -3711307621533570697L;
+
+    public ConceptGridUpdateRequestedEvent(final HierarchyView<Concept> source, final boolean fromClient) {
+        super(source, fromClient);
+    }
+}
