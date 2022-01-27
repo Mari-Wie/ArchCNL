2400c56a2fd9e58f7978125824a36ecace831bfd
Hendrik Zevenhuizen <Hendrik-Zevenhuizen@web.de>
2022-01-27 14:57:20 +0100

    interfaces introduced to support future implementations of arch-styles

---
+++ web-ui/src/main/java/org/archcnl/domain/input/model/presets/microservicearchitecture/ArchitecturalStyle.java
@@ -0,0 +1,10 @@
+package org.archcnl.domain.input.model.presets.microservicearchitecture;
+
+public interface ArchitecturalStyle {
+
+    void createRulesAndMappings();
+
+    void createMappings();
+
+    void createArchitecturalRules();
+}
---
+++ web-ui/src/main/java/org/archcnl/domain/input/model/presets/microservicearchitecture/ArchitecturalStyleTemplateManager.java
@@ -0,0 +1,8 @@
+package org.archcnl.domain.input.model.presets.microservicearchitecture;
+
+public interface ArchitecturalStyleTemplateManager {
+
+	/** Initializes the variables that are used within the mappings */
+	void initializeVariables();
+
+}
