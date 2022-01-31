fa2815e207c3f67cb749b74fb1595b5694713f35
Oliver Heidmann <oliver.heidmann@mpimet.mpg.de>
2022-01-31 16:52:17 +0100

    added new HierarchyElements to split editable from non editable parts

---
+++ web-ui/src/main/java/org/archcnl/ui/common/conceptandrelationlistview/EditableHierarchyView.java
@@ -0,0 +1,23 @@
+package org.archcnl.ui.common.conceptandrelationlistview;
+
+import org.archcnl.domain.common.HierarchyNode;
+import org.archcnl.domain.common.ObjectType;
+import org.archcnl.ui.events.EditorRequestedEvent;
+
+public class EditableHierarchyView<T extends ObjectType> extends HierarchyView {
+    public EditableHierarchyView() {
+        super();
+    }
+
+    @Override
+    public HierarchyEntryLayout createNewHierarchyEntry(HierarchyNode node) {
+        HierarchyEntryLayout<T> newLayout;
+        if (node.hasEntry() && node.getEntry().isEditable()) {
+            newLayout = new EditableHierarchyEntryLayout<T>(node);
+            newLayout.addListener(EditorRequestedEvent.class, this::fireEvent);
+        } else {
+            newLayout = new HierarchyEntryLayout<T>(node);
+        }
+        return newLayout;
+    }
+}
---
+++ web-ui/src/main/java/org/archcnl/ui/common/conceptandrelationlistview/HierarchyEntryLayout.java
@@ -0,0 +1,42 @@
+package org.archcnl.ui.common.conceptandrelationlistview;
+
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Unit;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.shared.Registration;
+import org.archcnl.domain.common.HierarchyNode;
+import org.archcnl.domain.common.ObjectType;
+
+public class HierarchyEntryLayout<T extends ObjectType> extends HorizontalLayout {
+
+    private static final long serialVersionUID = 2L;
+    private HierarchyNode<T> entry;
+
+    public HierarchyEntryLayout(final HierarchyNode<T> entry) {
+        this.entry = entry;
+        final Span text = new Span(entry.getName());
+        text.setWidth(100, Unit.PERCENTAGE);
+        addAndExpand(text);
+        updateDescription();
+    }
+
+    public HierarchyNode<T> getListEntry() {
+        return entry;
+    }
+
+    public T get() {
+        return entry.getEntry();
+    }
+
+    public void updateDescription() {
+        getElement().setAttribute("title", entry.getDescription());
+    }
+
+    @Override
+    public <T extends ComponentEvent<?>> Registration addListener(
+            final Class<T> eventType, final ComponentEventListener<T> listener) {
+        return getEventBus().addListener(eventType, listener);
+    }
+}
