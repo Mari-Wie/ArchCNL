ae7c4df3541680d4718da0cd3796a447f0b1f96b
Hendrik Zevenhuizen <Hendrik-Zevenhuizen@web.de>
2022-02-01 09:37:04 +0100

    initialize rules call added

--- web-ui/src/main/java/org/archcnl/domain/input/model/presets/microservicearchitecture/MicroserviceArchitectureTemplateManager.java
+++ web-ui/src/main/java/org/archcnl/domain/input/model/presets/microservicearchitecture/MicroserviceArchitectureTemplateManager.java
@@ -39,7 +39,7 @@ public class MicroserviceArchitectureTemplateManager implements ArchitecturalSty
     Variable methodVariable;
 
     public MicroserviceArchitectureTemplateManager() {
-        super();
+        initializeVariables();
     }
 
     @Override
