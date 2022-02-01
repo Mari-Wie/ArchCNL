226fdeb1e6ff6ff5afcf9c2e59adb9bc369ebba8
Hendrik Zevenhuizen <Hendrik-Zevenhuizen@web.de>
2022-02-01 16:26:43 +0100

    formatting

--- web-ui/src/main/java/org/archcnl/ui/inputview/presets/events/ArchitecturalStyleSaveEvent.java
+++ web-ui/src/main/java/org/archcnl/ui/inputview/presets/events/ArchitecturalStyleSaveEvent.java
@@ -2,8 +2,6 @@ package org.archcnl.ui.inputview.presets.events;
 
 import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.archcnl.domain.input.model.presets.ArchitecturalStyle;
@@ -11,29 +9,31 @@ import org.archcnl.ui.inputview.presets.ArchitecturalStyleForm;
 
 public class ArchitecturalStyleSaveEvent extends ComponentEvent<ArchitecturalStyleForm> {
 
-	private static final Logger LOG = LogManager.getLogger(ArchitecturalStyleSaveEvent.class);
-	private static final long serialVersionUID = 1L;
+    private static final Logger LOG = LogManager.getLogger(ArchitecturalStyleSaveEvent.class);
+    private static final long serialVersionUID = 1L;
 
-	private ArchitecturalStyle style;
+    private ArchitecturalStyle style;
 
-	public ArchitecturalStyleSaveEvent(ArchitecturalStyleForm architecturalStyleInputContainer, boolean fromClient,
-			ArchitecturalStyle architecturalStyles) {
-		super(architecturalStyleInputContainer, fromClient);
-		this.style = architecturalStyles;
-	}
+    public ArchitecturalStyleSaveEvent(
+            ArchitecturalStyleForm architecturalStyleInputContainer,
+            boolean fromClient,
+            ArchitecturalStyle architecturalStyles) {
+        super(architecturalStyleInputContainer, fromClient);
+        this.style = architecturalStyles;
+    }
 
-	public void handleEvent() {
-		switch (style) {
-		case MICROSERVICE_ARCHITECTURE:
-			getSource().mapUiComponentsToArchitecturalStyle(style);
-			break;
-		case LAYERED_ARCHITECTURE:
-			new Notification();
-			getSource().add(Notification.show("Not yet Implemented"));
-			ArchitecturalStyleSaveEvent.LOG.warn("{} is not implemented", style);
-			break;
-		default:
-			break;
-		}
-	}
+    public void handleEvent() {
+        switch (style) {
+            case MICROSERVICE_ARCHITECTURE:
+                getSource().mapUiComponentsToArchitecturalStyle(style);
+                break;
+            case LAYERED_ARCHITECTURE:
+                new Notification();
+                getSource().add(Notification.show("Not yet Implemented"));
+                ArchitecturalStyleSaveEvent.LOG.warn("{} is not implemented", style);
+                break;
+            default:
+                break;
+        }
+    }
 }
