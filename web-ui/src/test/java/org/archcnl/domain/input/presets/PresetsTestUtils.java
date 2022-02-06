c9278650b3bd51a6b0ee0dd794886ecf648dfb98
Hendrik Zevenhuizen <Hendrik-Zevenhuizen@web.de>
2022-02-06 14:57:52 +0100

    added test for architectural style configuration

--- web-ui/src/test/java/org/archcnl/domain/input/presets/PresetsTestUtils.java
+++ web-ui/src/test/java/org/archcnl/domain/input/presets/PresetsTestUtils.java
@@ -1,6 +1,8 @@
 package org.archcnl.domain.input.presets;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.archcnl.domain.input.model.presets.microservicearchitecture.ArchitecturalStyle;
 import org.archcnl.domain.input.model.presets.microservicearchitecture.MicroserviceArchitectureBuilder;
@@ -10,7 +12,18 @@ public class PresetsTestUtils {
 
     private static ArchitecturalStyle msa;
 
-    // constants that will be used in helper-methods and test-methods
+    // architecture rules
+    private static final String RUNTIME_ENV_RULE =
+            "Every Microservice must useown RuntimeEnvironment.";
+    private static final String API_GATEWAY_RULE =
+            "Every ApiGateway must resideinpackage MicroserviceApp.";
+    private static final String API_RULE = "Every Microservice must haveown API.";
+    private static final String CIRCUIT_BREAKER_RULE =
+            "Every Microservice must use CircuitBreaker.";
+    private static final String DECENTRALIZED_PERSISTENCE_RULE =
+            "Every Microservice must haveown DatabaseAccessAbstraction.";
+    private static final String SERVICE_REGISTRY_RULE =
+            "Every Microservice must registerin ServiceRegistry.";
 
     // first microservice
     private static final String MICROSERVICE_ONE_NAME = "MicroserviceOne";
@@ -60,6 +73,19 @@ public class PresetsTestUtils {
                         .build();
 
         return msa;
+    }
+
+    public static List<String> getMicroserviceArchitectureRules() {
+        List<String> knownRules = new ArrayList<String>();
+
+        knownRules.add(SERVICE_REGISTRY_RULE);
+        knownRules.add(DECENTRALIZED_PERSISTENCE_RULE);
+        knownRules.add(CIRCUIT_BREAKER_RULE);
+        knownRules.add(API_RULE);
+        knownRules.add(API_GATEWAY_RULE);
+        knownRules.add(RUNTIME_ENV_RULE);
+
+        return knownRules;
     }
 
     private static Set<TwoColumnGridEntry> createMicroservices() {
