b9c1542f6595308f264db9d65f0ee979ff87779a
Vincent Raudszus <vincent.raudszus@t-online.de>
2022-02-10 12:17:11 +0100

    Unused methods removed from DeleteHierarchyObjectRequestedEvent

--- web-ui/src/main/java/org/archcnl/ui/common/conceptandrelationlistview/events/DeleteHierarchyObjectRequestedEvent.java
+++ web-ui/src/main/java/org/archcnl/ui/common/conceptandrelationlistview/events/DeleteHierarchyObjectRequestedEvent.java
@@ -3,13 +3,11 @@ package org.archcnl.ui.common.conceptandrelationlistview.events;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentEvent;
 import org.archcnl.domain.common.conceptsandrelations.HierarchyObject;
-import org.archcnl.ui.common.conceptandrelationlistview.HierarchyView;
 
 public class DeleteHierarchyObjectRequestedEvent extends ComponentEvent<Component> {
 
     private static final long serialVersionUID = -8244752047463443057L;
     private HierarchyObject hierarchyObject;
-    private HierarchyView<HierarchyObject> hierarchyView;
 
     public DeleteHierarchyObjectRequestedEvent(
             Component component, boolean fromClient, HierarchyObject hierarchyObject) {
@@ -19,13 +17,5 @@ public class DeleteHierarchyObjectRequestedEvent extends ComponentEvent<Componen
 
     public HierarchyObject getHierarchyObject() {
         return hierarchyObject;
-    }
-
-    public HierarchyView<HierarchyObject> getHierarchyView() {
-        return hierarchyView;
-    }
-
-    public void setHierarchyView(HierarchyView<HierarchyObject> hierarchyView) {
-        this.hierarchyView = hierarchyView;
     }
 }
