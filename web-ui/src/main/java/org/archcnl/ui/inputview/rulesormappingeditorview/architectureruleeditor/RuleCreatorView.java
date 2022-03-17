ae0b123ec6d74702cbb653b394db648ca64bb356
cmkehler <christian.kehler@gmx.net>
2022-03-17 11:41:09 +0100

    (Cleanup) Moved modfierlist to the top, spotless

--- web-ui/src/main/java/org/archcnl/ui/inputview/rulesormappingeditorview/architectureruleeditor/RuleCreatorView.java
+++ web-ui/src/main/java/org/archcnl/ui/inputview/rulesormappingeditorview/architectureruleeditor/RuleCreatorView.java
@@ -87,13 +87,17 @@ public class RuleCreatorView extends RulesOrMappingEditorView {
 
     private void saveRule() {
         if (!archRuleTextArea.isEmpty()) {
-            fireEvent(new SaveRuleButtonPressedEvent(this, true, archRuleTextArea.getValue().stripTrailing() + "."));
+            fireEvent(
+                    new SaveRuleButtonPressedEvent(
+                            this, true, archRuleTextArea.getValue().stripTrailing() + "."));
             fireEvent(new RulesWidgetRequestedEvent(this, true));
             return;
         }
         fireEvent(
                 new SaveRuleButtonPressedEvent(
-                        this, true, (subject.getRuleString() + verb.getRuleString().stripTrailing() + ".")));
+                        this,
+                        true,
+                        (subject.getRuleString() + verb.getRuleString().stripTrailing() + ".")));
         fireEvent(new RulesWidgetRequestedEvent(this, true));
     }
 
