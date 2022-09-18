7b249fc913193e13f321ea15e7d492dff95183bb
Vincent Raudszus <vincent.raudszus@gmail.com>
2022-09-18 19:12:29 +0200

    Model class for SoftwareArtifactFile added

---
+++ web-ui/src/main/java/org/archcnl/domain/input/visualization/elements/SoftwareArtifaceFile.java
@@ -0,0 +1,36 @@
+package org.archcnl.domain.input.visualization.elements;
+
+import java.util.Optional;
+import org.archcnl.domain.common.conceptsandrelations.andtriplets.triplet.Variable;
+import org.archcnl.domain.input.visualization.exceptions.PropertyNotFoundException;
+
+public class SoftwareArtifaceFile implements PlantUmlElement {
+
+    private Variable variable;
+    private Optional<String> hasPath = Optional.empty();
+
+    public SoftwareArtifaceFile(Variable variable) {
+        this.variable = variable;
+    }
+
+    @Override
+    public String buildPlantUmlCode() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("note as " + variable.getName());
+        builder.append("\t===File");
+        if (hasPath.isPresent()) {
+            builder.append("\t" + hasPath.get());
+        }
+        builder.append("end note");
+        return builder.toString();
+    }
+
+    @Override
+    public void setProperty(String property, Object object) throws PropertyNotFoundException {
+        if ("hasPath".equals(property)) {
+            this.hasPath = Optional.of((String) object);
+        } else {
+            throw new PropertyNotFoundException(property + " couldn't be set");
+        }
+    }
+}
